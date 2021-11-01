1b2fe5300e7c10576534c23ebf4940cfd20a2047
Denis Kolegov <dnkolegov@gmail.com>
2021-11-01 23:11:00 +0700

    update tests

--- src/crypto.rs
+++ src/crypto.rs
@@ -1,7 +1,7 @@
 use ssz_rs::prelude::*;
 use rand::prelude::*;
 
-use blst::min_pk as blst;
+use blst::{min_pk as blst_core, BLST_ERROR};
 
 pub const BLS_PUBLIC_KEY_BYTES_LEN: usize = 48;
 pub const BLS_SECRET_KEY_BYTES_LEN: usize = 32;
@@ -10,17 +10,15 @@ pub const BLS_SIGNATURE_BYTES_LEN: usize = 96;
 pub type BLSPubkey = Vector<u8, BLS_PUBLIC_KEY_BYTES_LEN>;
 pub type BLSSignature = Vector<u8, BLS_SIGNATURE_BYTES_LEN>;
 
-pub struct Signature(pub(crate) blst::Signature);
-
 #[derive(Debug)]
-pub struct SecretKey(pub(crate) blst::SecretKey);
+pub struct SecretKey(pub(crate) blst_core::SecretKey);
 
 impl SecretKey {
     pub fn random() -> Self {
         let mut ikm=[0u8; 32];
         let mut rng = rand::thread_rng();
-        rng.fill_bytes(&mut ikm);
-        let sk = blst::SecretKey::key_gen(&ikm, &[]).expect("unable to generate a secret key");
+        rng.try_fill_bytes(&mut ikm).expect("unable to generate random data");
+        let sk = blst_core::SecretKey::key_gen(&ikm, &[]).expect("unable to generate a secret key");
         SecretKey(sk)
     }
 
@@ -36,9 +34,47 @@ impl SecretKey {
 }
 
 #[derive(Debug)]
-pub struct PublicKey(pub(crate) blst::PublicKey);
+pub struct PublicKey(pub(crate) blst_core::PublicKey);
 
 impl PublicKey {
+    pub fn verify(&self, sig: Signature, msg: &[u8]) -> bool {
+        let dst = b"BLS_SIG_BLS12381G2_XMD:SHA-256_SSWU_RO_NUL_";
+        let pk = self.0;
+        let avg = &[];
+        let err = sig.0.verify(true, msg, dst, avg, &pk, true);
+        err == BLST_ERROR::BLST_SUCCESS
+    }
+}
+
+#[derive(Debug)]
+pub struct Signature(pub(crate) blst_core::Signature);
+
+impl Signature {
+    pub fn verify(&self, pk: PublicKey, msg: &[u8]) -> bool {
+        let dst = b"BLS_SIG_BLS12381G2_XMD:SHA-256_SSWU_RO_NUL_";
+        let avg = &[];
+        let err = self.0.verify(true, msg, dst, avg, &pk.0, true);
+        err == BLST_ERROR::BLST_SUCCESS
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::SecretKey;
+
+    #[test]
+    fn signature() {
+        let sk = SecretKey::random();
+        let pk = sk.public_key();
+        let msg = "message";
+        let sig = sk.sign(msg.as_ref());
+
+        assert!(sig.verify(pk, msg.as_ref()));
+
+        let pk = sk.public_key();
+        assert!(pk.verify(sig, msg.as_ref()));
+    }
+
 
 }
 
