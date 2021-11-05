398e76cf41c0c38c2d01b2518b7aca786c6d742d
Alex Stokes <r.alex.stokes@gmail.com>
2021-11-05 16:49:54 +0100

    re-org `state_transition` and add some helper functions

--- src/phase0/state_transition/mod.rs
+++ src/phase0/state_transition/mod.rs
@@ -1,16 +1,128 @@
+mod block_processing;
+mod epoch_processing;
+
+use crate::crypto::fast_aggregate_verify;
 use crate::domains::{DomainType, SigningData};
 use crate::phase0::beacon_block::SignedBeaconBlock;
 use crate::phase0::beacon_state::BeaconState;
-use crate::primitives::{Domain, Epoch, Root, Version};
-use ssz_rs::{MerkleizationContext, Merkleized, SimpleSerialize};
+use crate::phase0::operations::{AttestationData, IndexedAttestation};
+use crate::phase0::validator::Validator;
+use crate::primitives::{Domain, Epoch, Gwei, Root, Version, FAR_FUTURE_EPOCH};
+use ssz_rs::prelude::*;
+use std::collections::HashSet;
 use thiserror::Error;
+
+pub fn is_active_validator(validator: Validator, epoch: Epoch) -> bool {
+    validator.activation_epoch <= epoch && epoch < validator.exit_epoch
+}
+
+pub fn is_eligible_for_activation_queue<const MAX_EFFECTIVE_BALANCE: Gwei>(
+    validator: Validator,
+) -> bool {
+    validator.activation_eligibility_epoch == FAR_FUTURE_EPOCH
+        && validator.effective_balance == MAX_EFFECTIVE_BALANCE
+}
+
+pub fn is_eligible_for_activation<
+    const SLOTS_PER_HISTORICAL_ROOT: usize,
+    const HISTORICAL_ROOTS_LIMIT: usize,
+    const ETH1_DATA_VOTES_BOUND: usize,
+    const VALIDATOR_REGISTRY_LIMIT: usize,
+    const EPOCHS_PER_HISTORICAL_VECTOR: usize,
+    const EPOCHS_PER_SLASHINGS_VECTOR: usize,
+    const MAX_VALIDATORS_PER_COMMITTEE: usize,
+    const PENDING_ATTESTATIONS_BOUND: usize,
+>(
+    state: BeaconState<
+        SLOTS_PER_HISTORICAL_ROOT,
+        HISTORICAL_ROOTS_LIMIT,
+        ETH1_DATA_VOTES_BOUND,
+        VALIDATOR_REGISTRY_LIMIT,
+        EPOCHS_PER_HISTORICAL_VECTOR,
+        EPOCHS_PER_SLASHINGS_VECTOR,
+        MAX_VALIDATORS_PER_COMMITTEE,
+        PENDING_ATTESTATIONS_BOUND,
+    >,
+    validator: Validator,
+) -> bool {
+    validator.activation_eligibility_epoch <= state.finalized_checkpoint.epoch
+        && validator.activation_epoch == FAR_FUTURE_EPOCH
+}
+
+pub fn is_slashable_validator(validator: Validator, epoch: Epoch) -> bool {
+    !validator.slashed
+        && validator.activation_epoch <= epoch
+        && epoch < validator.withdrawable_epoch
+}
+
+pub fn is_slashable_attestation_data(data_1: AttestationData, data_2: AttestationData) -> bool {
+    let double_vote = data_1 != data_2 && data_1.target.epoch == data_2.target.epoch;
+    let surround_vote =
+        data_1.source.epoch < data_2.source.epoch && data_2.target.epoch < data_1.target.epoch;
+    double_vote || surround_vote
+}
+
+pub fn is_valid_indexed_attestation<
+    const SLOTS_PER_HISTORICAL_ROOT: usize,
+    const HISTORICAL_ROOTS_LIMIT: usize,
+    const ETH1_DATA_VOTES_BOUND: usize,
+    const VALIDATOR_REGISTRY_LIMIT: usize,
+    const EPOCHS_PER_HISTORICAL_VECTOR: usize,
+    const EPOCHS_PER_SLASHINGS_VECTOR: usize,
+    const MAX_VALIDATORS_PER_COMMITTEE: usize,
+    const PENDING_ATTESTATIONS_BOUND: usize,
+>(
+    state: BeaconState<
+        SLOTS_PER_HISTORICAL_ROOT,
+        HISTORICAL_ROOTS_LIMIT,
+        ETH1_DATA_VOTES_BOUND,
+        VALIDATOR_REGISTRY_LIMIT,
+        EPOCHS_PER_HISTORICAL_VECTOR,
+        EPOCHS_PER_SLASHINGS_VECTOR,
+        MAX_VALIDATORS_PER_COMMITTEE,
+        PENDING_ATTESTATIONS_BOUND,
+    >,
+    indexed_attestation: IndexedAttestation<MAX_VALIDATORS_PER_COMMITTEE>,
+) -> Result<(), Error> {
+    if indexed_attestation.attesting_indices.is_empty() {
+        return Err(Error::InvalidOperation);
+    }
+    let indices: HashSet<usize> =
+        HashSet::from_iter(indexed_attestation.attesting_indices.iter().cloned());
+    if indices.len() != indexed_attestation.attesting_indices.len() {
+        return Err(Error::InvalidOperation);
+    }
+    let pubkeys = state.validators.iter().enumerate().filter_map(|(i, v)| {
+        if indices.contains(&i) {
+            Some(&v.pubkey)
+        } else {
+            None
+        }
+    });
+
+    let domain = get_domain(
+        &state,
+        DomainType::BeaconAttester,
+        Some(indexed_attestation.data.target.epoch),
+    );
+    let signing_root = compute_signing_root(&indexed_attestation.data, domain)?;
+    if fast_aggregate_verify(pubkeys, signing_root, &indexed_attestation.signature) {
+        Ok(())
+    } else {
+        Err(Error::InvalidSignature)
+    }
+}
 
 #[derive(Debug, Error)]
 pub enum Error {
     #[error("Block Signature Error")]
     BlockSignatureError,
     #[error("Merkleization Error")]
-    MerkleizationError(#[from] ssz_rs::MerkleizationError),
+    MerkleizationError(#[from] MerkleizationError),
+    #[error("invalid operation")]
+    InvalidOperation,
+    #[error("invalid signature")]
+    InvalidSignature,
 }
 
 pub fn apply_block<
@@ -99,7 +211,7 @@ pub fn verify_block_signature<
         .get(proposer_index)
         .expect("failed to get validator");
     let signing_root = match compute_signing_root(
-        signed_block.message,
+        &signed_block.message,
         get_domain(&state, DomainType::BeaconProposer, None),
     ) {
         Ok(root) => root,
@@ -108,7 +220,7 @@ pub fn verify_block_signature<
 
     proposer
         .pubkey
-        .verify_signature(&signing_root.as_ref()[..], signed_block.signature)
+        .verify_signature(signing_root, signed_block.signature)
 }
 
 pub fn get_domain<
@@ -185,7 +297,7 @@ pub fn get_current_epoch<
 }
 
 pub fn compute_signing_root<T: SimpleSerialize>(
-    ssz_object: T,
+    ssz_object: &T,
     domain: Domain,
 ) -> Result<Root, Error> {
     let context = MerkleizationContext::new();
