45b7463082c97790f7a16f5bd5e0cf2dcb43ad0d
Denis Kolegov <dnkolegov@gmail.com>
2021-11-06 18:04:22 +0700

    add misc funcs

--- src/domains.rs
+++ src/domains.rs
@@ -4,11 +4,25 @@ use ssz_rs::prelude::*;
 pub enum DomainType {
     BeaconProposer,
     BeaconAttester,
-    RANDAO,
+    Randao,
     Deposit,
     VoluntaryExit,
     SelectionProof,
     AggregateAndProof,
+}
+
+impl DomainType {
+    pub fn get_domain_constant(&self) -> [u8; 4] {
+        match self {
+            DomainType::BeaconProposer => 0u32.to_le_bytes(),
+            DomainType::BeaconAttester => 1u32.to_le_bytes(),
+            DomainType::Randao => 2u32.to_le_bytes(),
+            DomainType::Deposit => 3u32.to_le_bytes(),
+            DomainType::VoluntaryExit => 4u32.to_le_bytes(),
+            DomainType::SelectionProof => 5u32.to_le_bytes(),
+            DomainType::AggregateAndProof => 6u32.to_le_bytes(),
+        }
+    }
 }
 
 #[derive(Default, Debug, SimpleSerialize)]
