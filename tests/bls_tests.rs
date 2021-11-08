aaf3b4611dac9f8abc8c93033d50d32135a9b2de
Pushkar Kopparla <pushkarkopparla@gmail.com>
2021-11-08 16:57:39 +0100

    Tests for fast_aggregate_verify, verify and sign

--- tests/bls_tests.rs
+++ tests/bls_tests.rs
@@ -1,4 +1,6 @@
-use ethereum_consensus::crypto::{aggregate, aggregate_verify, PublicKey, SecretKey, Signature};
+use ethereum_consensus::crypto::{
+    aggregate, aggregate_verify, fast_aggregate_verify, PublicKey, SecretKey, Signature,
+};
 use glob::glob;
 use hex;
 use serde::{Deserialize, Serialize};
@@ -73,38 +75,135 @@ struct AggVerifyTestIO {
     output: bool,
 }
 
-impl AggVerifyTestIO{
+impl AggVerifyTestIO {
     fn aggregate_verify(&self) -> bool {
         let pubkeys: Vec<PublicKey> = self
-        .input
-        .pubkeys
-        .iter()
-        .map(|x| PublicKey::from_bytes(&decode_hex_with_prefix(&x)))
-        .collect();
-    let messages_vec: Vec<_> = self
-        .input
-        .messages
-        .iter()
-        .map(|x| decode_hex_with_prefix(x))
-        .collect();
-    let messages_slice: Vec<&[u8]> = messages_vec.iter().map(|x| x.as_slice()).collect();
-    let signature: Signature =
-        Signature::from_bytes(&decode_hex_with_prefix(&self.input.signature));
-    let verify_result = aggregate_verify(&pubkeys, messages_slice.as_ref(), signature);
-    verify_result == self.output
+            .input
+            .pubkeys
+            .iter()
+            .map(|x| PublicKey::from_bytes(&decode_hex_with_prefix(&x)))
+            .collect();
+        let messages_vec: Vec<_> = self
+            .input
+            .messages
+            .iter()
+            .map(|x| decode_hex_with_prefix(x))
+            .collect();
+        let messages_slice: Vec<&[u8]> = messages_vec.iter().map(|x| x.as_slice()).collect();
+        let signature: Signature =
+            Signature::from_bytes(&decode_hex_with_prefix(&self.input.signature));
+        let verify_result = aggregate_verify(&pubkeys, messages_slice.as_ref(), signature);
+        verify_result == self.output
     }
 }
+#[derive(Debug, Serialize, Deserialize)]
+
+struct FastAggVerifyInput {
+    pubkeys: Vec<String>,
+    message: String,
+    signature: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct FastAggVerifyTestIO {
+    input: FastAggVerifyInput,
+    output: bool,
+}
+impl FastAggVerifyTestIO {
+    fn fast_aggregate_verify(&self) -> bool {
+        let pubkeys: Vec<PublicKey> = self
+            .input
+            .pubkeys
+            .iter()
+            .map(|x| PublicKey::from_bytes(&decode_hex_with_prefix(&x)))
+            .collect();
+        let message_slice: &[u8] = &decode_hex_with_prefix(&self.input.message);
+        let signature: Signature =
+            Signature::from_bytes(&decode_hex_with_prefix(&self.input.signature));
+        let verify_result = fast_aggregate_verify(&pubkeys, message_slice, signature);
+        verify_result == self.output
+    }
+}
+#[derive(Debug, Serialize, Deserialize)]
+
+struct VerifyInput {
+    pubkey: String,
+    message: String,
+    signature: String,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+
+struct VerifyTestIO {
+    input: VerifyInput,
+    output: bool,
+}
+
+impl VerifyTestIO {
+    fn verify(&self) -> bool {
+        let pubkey: PublicKey = PublicKey::from_bytes(&decode_hex_with_prefix(&self.input.pubkey));
+        let message_vec: Vec<u8> = decode_hex_with_prefix(&self.input.message);
+        let message: &[u8] = message_vec.as_ref();
+        let signature: Signature =
+            Signature::from_bytes(&decode_hex_with_prefix(&self.input.signature));
+        let verify_result = signature.verify(pubkey, message);
+        verify_result == self.output
+    }
+}
+
 #[test]
 #[cfg(feature = "ef_spec_tests")]
-fn test_aggverify() {
-    for entry in glob("consensus-spec-tests/tests/general/phase0/bls/aggregate_verify/small/**/*.yaml")
+fn test_fast_aggregate_verify() {
+    for entry in
+        glob("consensus-spec-tests/tests/general/phase0/bls/fast_aggregate_verify/small/**/*.yaml")
+            .expect("Failed to read glob pattern")
+    {
+        match entry {
+            Ok(path) => {
+                println!("{:?}", path.display());
+                let file = File::open(path).expect("file exists");
+                let test_case: FastAggVerifyTestIO =
+                    serde_yaml::from_reader(file).expect("is well-formatted yaml");
+                assert!(test_case.fast_aggregate_verify())
+            }
+            Err(e) => println!("{:?}", e),
+        }
+    }
+}
+
+#[test]
+#[ignore]
+#[cfg(feature = "ef_spec_tests")]
+fn test_verify() {
+    for entry in glob("consensus-spec-tests/tests/general/phase0/bls/verify/small/**/*.yaml")
         .expect("Failed to read glob pattern")
     {
         match entry {
             Ok(path) => {
                 println!("{:?}", path.display());
                 let file = File::open(path).expect("file exists");
-                let test_case: AggVerifyTestIO = serde_yaml::from_reader(file).expect("is well-formatted yaml");
+                let test_case: VerifyTestIO =
+                    serde_yaml::from_reader(file).expect("is well-formatted yaml");
+                assert!(test_case.verify())
+            }
+            Err(e) => println!("{:?}", e),
+        }
+    }
+}
+#[test]
+#[ignore]
+#[cfg(feature = "ef_spec_tests")]
+fn test_aggregate_verify() {
+    for entry in
+        glob("consensus-spec-tests/tests/general/phase0/bls/aggregate_verify/small/**/*.yaml")
+            .expect("Failed to read glob pattern")
+    {
+        match entry {
+            Ok(path) => {
+                println!("{:?}", path.display());
+                let file = File::open(path).expect("file exists");
+                let test_case: AggVerifyTestIO =
+                    serde_yaml::from_reader(file).expect("is well-formatted yaml");
                 // println!("{}",test_case.output);
                 // println!("{:?}",test_case.input.messages);
                 assert!(test_case.aggregate_verify())
@@ -118,7 +217,7 @@ fn test_aggverify() {
 #[ignore]
 #[cfg(feature = "ef_spec_tests")]
 
-fn test_aggregating() {
+fn test_aggregate() {
     for entry in glob("consensus-spec-tests/tests/general/phase0/bls/aggregate/small/**/*.yaml")
         .expect("Failed to read glob pattern")
     {
@@ -138,7 +237,7 @@ fn test_aggregating() {
 #[ignore]
 #[cfg(feature = "ef_spec_tests")]
 
-fn test_signing() {
+fn test_sign() {
     for entry in glob("consensus-spec-tests/tests/general/phase0/bls/sign/small/**/*.yaml")
         .expect("Failed to read glob pattern")
     {
