db8aa3819fbe3cdf9736b869e9ebbafe67c3fe94
Alex Stokes <r.alex.stokes@gmail.com>
2021-12-08 12:30:09 +0100

    add stub for `process_deposit` block processing

--- src/phase0/state_transition/block_processing.rs
+++ src/phase0/state_transition/block_processing.rs
@@ -1,1 +1,29 @@
+use crate::phase0::beacon_state::BeaconState;
+use crate::phase0::operations::Deposit;
+use crate::phase0::state_transition::{Context, Error};
 
+pub fn process_deposit<
+    const SLOTS_PER_HISTORICAL_ROOT: usize,
+    const HISTORICAL_ROOTS_LIMIT: usize,
+    const ETH1_DATA_VOTES_BOUND: usize,
+    const VALIDATOR_REGISTRY_LIMIT: usize,
+    const EPOCHS_PER_HISTORICAL_VECTOR: usize,
+    const EPOCHS_PER_SLASHINGS_VECTOR: usize,
+    const MAX_VALIDATORS_PER_COMMITTEE: usize,
+    const PENDING_ATTESTATIONS_BOUND: usize,
+>(
+    state: &mut BeaconState<
+        SLOTS_PER_HISTORICAL_ROOT,
+        HISTORICAL_ROOTS_LIMIT,
+        ETH1_DATA_VOTES_BOUND,
+        VALIDATOR_REGISTRY_LIMIT,
+        EPOCHS_PER_HISTORICAL_VECTOR,
+        EPOCHS_PER_SLASHINGS_VECTOR,
+        MAX_VALIDATORS_PER_COMMITTEE,
+        PENDING_ATTESTATIONS_BOUND,
+    >,
+    deposit: &Deposit,
+    context: &Context,
+) -> Result<(), Error> {
+    Ok(())
+}
