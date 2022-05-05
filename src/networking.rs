6bbbe5f2d86f8efe6781ccbf8fc4632b4b15dcfe
Alex Stokes <r.alex.stokes@gmail.com>
2022-05-05 17:54:28 -0600

    add `serde` support for some networking types

--- src/networking.rs
+++ src/networking.rs
@@ -32,7 +32,9 @@ impl MessageDomain {
     }
 }
 
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
 pub struct MetaData {
+    #[serde(with = "crate::serde::as_string")]
     pub seq_number: u64,
     pub attnets: Bitvector<ATTESTATION_SUBNET_COUNT>,
 }
