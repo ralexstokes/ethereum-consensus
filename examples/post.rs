92f976ee8ae592f925a2405bea88640edf185330
Alex Stokes <r.alex.stokes@gmail.com>
2022-05-14 17:17:10 -0600

    add example for `http_post`

---
+++ examples/post.rs
@@ -0,0 +1,16 @@
+use beacon_api_client::Client;
+use ethereum_consensus::builder::SignedValidatorRegistration;
+use url::Url;
+
+#[tokio::main]
+async fn main() {
+    let client = Client::new(Url::parse("http://localhost:8080").unwrap());
+    let data = SignedValidatorRegistration::default();
+    let response = client
+        .http_post("/eth/v1/builder/validators", &data)
+        .await
+        .unwrap();
+    dbg!(&response);
+    dbg!(&response.status());
+    dbg!(&response.text().await);
+}
