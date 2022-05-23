ba4344786dc5d61f62366a494c259dacf4f1f1f6
Alex Stokes <r.alex.stokes@gmail.com>
2022-05-22 20:32:24 -0600

    work towards streamlined testing pattern

--- tests/bls_tests.rs
+++ tests/bls_tests.rs
@@ -8,12 +8,14 @@ use ethereum_consensus::crypto::{
 use ethereum_consensus::primitives::Bytes32;
 use ethereum_consensus::serde as eth_serde;
 use serde::Deserialize;
+use serde_with::{serde_as, DefaultOnError};
 use test_utils::TestCase;
 
+#[serde_as]
 #[derive(Debug, Deserialize)]
 struct SignInput {
-    #[serde(deserialize_with = "eth_serde::as_hex::deserialize")]
-    privkey: Vec<u8>,
+    #[serde_as(deserialize_as = "DefaultOnError")]
+    privkey: Option<SecretKey>,
     #[serde(deserialize_with = "eth_serde::as_hex::deserialize")]
     message: Vec<u8>,
 }
@@ -25,15 +27,22 @@ struct SignTest {
 }
 
 impl TestCase for SignTest {
-    fn execute(self) {
-        match self.output {
-            Some(output) => {
-                let secret_key = SecretKey::from_bytes(&self.input.privkey).unwrap();
-                let signature = secret_key.sign(&self.input.message);
-                assert_eq!(signature, output)
-            }
-            None => assert!(SecretKey::from_bytes(&self.input.privkey).is_err()),
-        }
+    fn should_succeed(&self) -> bool {
+        self.output.is_some()
+    }
+
+    fn verify_success(&self) -> bool {
+        let signature = self
+            .input
+            .privkey
+            .as_ref()
+            .unwrap()
+            .sign(self.input.message.as_ref());
+        &signature == self.output.as_ref().unwrap()
+    }
+
+    fn verify_failure(&self) -> bool {
+        self.input.privkey.is_none()
     }
 }
 
@@ -44,20 +53,29 @@ struct AggregateTest {
 }
 
 impl TestCase for AggregateTest {
-    fn execute(self) {
+    fn should_succeed(&self) -> bool {
+        self.output.is_some()
+    }
+
+    fn verify_success(&self) -> bool {
+        let aggregation = aggregate(&self.input).unwrap();
+        self.output.as_ref().unwrap() == &aggregation
+    }
+
+    fn verify_failure(&self) -> bool {
         let aggregation = aggregate(&self.input);
-        match self.output {
-            Some(output) => assert_eq!(output, aggregation.unwrap()),
-            None => assert!(aggregation.is_err()),
-        }
+        aggregation.is_err()
     }
 }
 
+#[serde_as]
 #[derive(Debug, Deserialize)]
 struct AggregateVerifyInput {
-    pubkeys: Vec<String>,
+    #[serde_as(deserialize_as = "DefaultOnError")]
+    pubkeys: Vec<Option<PublicKey>>,
     messages: Vec<Bytes32>,
-    signature: String,
+    #[serde_as(deserialize_as = "DefaultOnError")]
+    signature: Option<Signature>,
 }
 
 #[derive(Debug, Deserialize)]
@@ -66,35 +84,50 @@ struct AggregateVerifyTest {
     output: bool,
 }
 
-impl TestCase for AggregateVerifyTest {
-    fn execute(self) {
-        let signature = self.input.signature.try_into();
-        if self.output {
-            let public_keys = self
-                .input
-                .pubkeys
-                .into_iter()
-                .map(|key| PublicKey::try_from(key).unwrap())
-                .collect::<Vec<_>>();
-            let messages = self
-                .input
-                .messages
-                .iter()
-                .map(|m| m.as_ref())
-                .collect::<Vec<_>>();
-            let aggregation = aggregate_verify(&public_keys, &messages, signature.unwrap());
-            assert!(aggregation);
-        } else {
-            assert!(signature.is_err())
-        }
+impl AggregateVerifyTest {
+    fn run(&self) -> bool {
+        let public_keys = self
+            .input
+            .pubkeys
+            .iter()
+            .cloned()
+            .filter_map(|k| k)
+            .collect::<Vec<_>>();
+        let messages = self
+            .input
+            .messages
+            .iter()
+            .map(|m| m.as_ref())
+            .collect::<Vec<_>>();
+        let signature = self.input.signature.as_ref().unwrap();
+        aggregate_verify(&public_keys, &messages, signature)
     }
 }
 
+impl TestCase for AggregateVerifyTest {
+    fn should_succeed(&self) -> bool {
+        self.output
+    }
+
+    fn verify_success(&self) -> bool {
+        self.run()
+    }
+
+    fn verify_failure(&self) -> bool {
+        if self.input.signature.is_none() {
+            return true;
+        }
+        !self.run()
+    }
+}
+
+#[serde_as]
 #[derive(Debug, Deserialize)]
 struct VerifyInput {
-    pubkey: String,
+    pubkey: PublicKey,
     message: Bytes32,
-    signature: Signature,
+    #[serde_as(deserialize_as = "DefaultOnError")]
+    signature: Option<Signature>,
 }
 
 #[derive(Debug, Deserialize)]
@@ -103,26 +136,40 @@ struct VerifyTest {
     output: bool,
 }
 
-impl TestCase for VerifyTest {
-    fn execute(self) {
-        let success = PublicKey::try_from(self.input.pubkey).map(|public_key| {
-            self.input
-                .signature
-                .verify(&public_key, self.input.message.as_ref())
-        });
-        if self.output {
-            assert!(success.unwrap());
-        } else {
-            assert!(success.is_err());
-        }
+impl VerifyTest {
+    fn run(&self) -> bool {
+        self.input
+            .signature
+            .as_ref()
+            .unwrap()
+            .verify(&self.input.pubkey, self.input.message.as_ref())
     }
 }
 
+impl TestCase for VerifyTest {
+    fn should_succeed(&self) -> bool {
+        self.output
+    }
+
+    fn verify_success(&self) -> bool {
+        self.run()
+    }
+
+    fn verify_failure(&self) -> bool {
+        if self.input.signature.is_none() {
+            return true;
+        }
+        !self.run()
+    }
+}
+
+#[serde_as]
 #[derive(Debug, Deserialize)]
 struct FastAggregateVerifyInput {
     pubkeys: Vec<PublicKey>,
     message: Bytes32,
-    signature: Signature,
+    #[serde_as(deserialize_as = "DefaultOnError")]
+    signature: Option<Signature>,
 }
 
 #[derive(Debug, Deserialize)]
@@ -131,15 +178,40 @@ struct FastAggregateVerifyTest {
     output: bool,
 }
 
-impl TestCase for FastAggregateVerifyTest {
-    fn execute(self) {
+impl FastAggregateVerifyTest {
+    fn run(&self) -> bool {
         let public_keys = self.input.pubkeys.iter().collect::<Vec<_>>();
-        let success = fast_aggregate_verify(
+        fast_aggregate_verify(
             &public_keys,
             self.input.message.as_ref(),
-            &self.input.signature,
-        );
-        assert_eq!(success, self.output);
+            self.input.signature.as_ref().unwrap(),
+        )
+    }
+}
+
+impl TestCase for FastAggregateVerifyTest {
+    fn should_succeed(&self) -> bool {
+        self.output
+    }
+
+    fn verify_success(&self) -> bool {
+        self.run()
+    }
+
+    fn verify_failure(&self) -> bool {
+        if self.input.signature.is_none() {
+            return true;
+        }
+        if self
+            .input
+            .pubkeys
+            .iter()
+            .any(|k| k == &PublicKey::default())
+        {
+            return true;
+        }
+
+        !self.run()
     }
 }
 
--- tests/test_utils.rs
+++ tests/test_utils.rs
@@ -1,20 +1,40 @@
 use glob::glob;
+use serde::Deserialize;
+use std::fmt;
 use std::fs::File;
 
 pub trait TestCase
 where
-    Self: for<'de> serde::Deserialize<'de>,
+    Self: for<'de> Deserialize<'de> + fmt::Debug,
 {
-    fn execute(self);
+    fn should_succeed(&self) -> bool;
+
+    fn verify_success(&self) -> bool;
+
+    fn verify_failure(&self) -> bool;
+
+    fn execute(&self) {
+        let result = if self.should_succeed() {
+            self.verify_success()
+        } else {
+            self.verify_failure()
+        };
+        assert!(result, "{:#?}", self)
+    }
 
     fn execute_from_glob(path_glob: &str) {
         let entries = glob(path_glob).expect("Failed to read glob pattern");
         for entry in entries {
             let path = entry.unwrap();
-            let mut file = File::open(path).expect("File does not exist");
-            let test_case: Self =
-                serde_yaml::from_reader(&mut file).expect("Is not well-formatted yaml");
-            test_case.execute()
+            let mut file = File::open(&path).expect("File does not exist");
+            let test_case: Result<Self, _> = serde_yaml::from_reader(&mut file);
+            match test_case {
+                Ok(test_case) => test_case.execute(),
+                Err(err) => {
+                    let content = std::fs::read_to_string(&path).unwrap();
+                    panic!("{err} from {content} at {path:?}")
+                }
+            }
         }
     }
 }
