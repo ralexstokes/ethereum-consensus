298c39fe20eb676604efaf5d43fdd80c0dba487f
Alex Stokes <r.alex.stokes@gmail.com>
2022-05-25 13:59:34 -0600

    update example to display new specs

--- examples/sketch.rs
+++ examples/sketch.rs
@@ -1,21 +1,32 @@
-use ethereum_consensus::altair::mainnet::SyncCommittee;
-use ethereum_consensus::bellatrix::mainnet::{ExecutionPayload, ExecutionPayloadHeader};
-use ethereum_consensus::phase0::mainnet::{self as spec, BeaconState, SignedBeaconBlock};
+use ethereum_consensus::altair::mainnet::{self as altair};
+use ethereum_consensus::bellatrix::mainnet as bellatrix;
+use ethereum_consensus::phase0::mainnet::{self as phase0};
 use ethereum_consensus::state_transition::{Context, Validation};
 
 fn main() {
     let context = Context::for_mainnet();
-    let mut state = BeaconState::default();
-    let mut signed_block = SignedBeaconBlock::default();
+    let mut state = phase0::BeaconState::default();
+    let mut signed_block = phase0::SignedBeaconBlock::default();
     signed_block.message.slot = 1;
 
-    let previous_epoch = spec::get_previous_epoch(&state, &context);
+    let previous_epoch = phase0::get_previous_epoch(&state, &context);
     dbg!(previous_epoch);
 
-    let _ = spec::state_transition(&mut state, &mut signed_block, Validation::Enabled, &context);
+    let _ = phase0::state_transition(&mut state, &mut signed_block, Validation::Enabled, &context);
     dbg!(state.fork);
 
-    dbg!(SyncCommittee::default());
-    dbg!(ExecutionPayload::default());
-    dbg!(ExecutionPayloadHeader::default());
+    let mut state = altair::BeaconState::default();
+    let mut signed_block = altair::SignedBeaconBlock::default();
+    state.slot = 32;
+    signed_block.message.slot = 33;
+
+    let current_epoch = altair::get_current_epoch(&state, &context);
+    dbg!(current_epoch);
+
+    let _ = altair::state_transition(&mut state, &mut signed_block, Validation::Enabled, &context);
+    dbg!(state.fork);
+
+    dbg!(altair::SyncCommittee::default());
+    dbg!(bellatrix::ExecutionPayload::default());
+    dbg!(bellatrix::ExecutionPayloadHeader::default());
 }
