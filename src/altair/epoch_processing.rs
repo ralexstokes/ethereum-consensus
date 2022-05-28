c6dc663a72d92b4b6dfaada8aa995f7806fcd7ce
Alex Stokes <r.alex.stokes@gmail.com>
2022-05-28 09:25:11 -0600

    clean up `altair::process_epoch`

--- src/altair/epoch_processing.rs
+++ src/altair/epoch_processing.rs
@@ -1,11 +1,11 @@
-// TODO remove once impl is added
-#![allow(dead_code)]
-#![allow(unused_imports)]
-
 use crate::altair as spec;
 
 use crate::state_transition::{Context, Result};
-use spec::{process_slashings, BeaconState};
+use spec::{
+    process_effective_balance_updates, process_eth1_data_reset, process_historical_roots_update,
+    process_randao_mixes_reset, process_registry_updates, process_slashings,
+    process_slashings_reset, BeaconState,
+};
 
 pub fn process_justification_and_finalization<
     const SLOTS_PER_HISTORICAL_ROOT: usize,
@@ -157,13 +157,13 @@ pub fn process_epoch<
     process_justification_and_finalization(state, context)?;
     process_inactivity_updates(state, context)?;
     process_rewards_and_penalties(state, context)?;
-    // process_registry_updates(state, context);
+    process_registry_updates(state, context);
     process_slashings(state, context)?;
-    // process_eth1_data_reset(state, context);
-    // process_effective_balance_updates(state, context);
-    // process_slashings_reset(state, context);
-    // process_randao_mixes_reset(state, context);
-    // process_historical_roots_update(state, context)?;
+    process_eth1_data_reset(state, context);
+    process_effective_balance_updates(state, context);
+    process_slashings_reset(state, context);
+    process_randao_mixes_reset(state, context);
+    process_historical_roots_update(state, context)?;
     process_participation_flag_updates(state, context)?;
     process_sync_committee_updates(state, context)?;
     Ok(())
