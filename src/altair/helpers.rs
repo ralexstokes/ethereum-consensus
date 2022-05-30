e145597ee8d8f8292e713c5f8a3f3b213c0311bd
Dan Buchholz <buchhlz2@gmail.com>
2022-05-30 13:11:56 -0700

    feat: add altair helpers - Finished get_unslashed_participating_indices - Added impl for get_attestation_participation_flag_indices, get_flag_index_deltas

--- src/altair/helpers.rs
+++ src/altair/helpers.rs
@@ -1,18 +1,20 @@
-// TODO remove once impl is added
-#![allow(dead_code)]
-
 use crate::altair as spec;
 
-use crate::crypto::{ hash, eth_aggregate_pubkeys};
-use crate::primitives::{Epoch, Gwei, ParticipationFlags, ValidatorIndex};
-use crate::state_transition::{Context, Error, Result};
+use crate::crypto::{eth_aggregate_pubkeys, hash};
 use crate::domains::DomainType;
-use spec::{
-    decrease_balance, get_beacon_proposer_index, get_current_epoch, get_eligible_validator_indices,
-    get_previous_epoch, increase_balance, initiate_validator_exit, BeaconState, sync::SyncCommittee as SyncCommittee, get_active_validator_indices, get_seed, compute_shuffled_index, get_total_active_balance
+use crate::primitives::{Epoch, Gwei, ParticipationFlags, ValidatorIndex};
+use crate::state_transition::{
+    invalid_operation_error, Context, Error, InvalidAttestation, InvalidOperation, Result,
 };
-use ssz_rs::{List, Vector};
 use integer_sqrt::IntegerSquareRoot;
+use spec::{
+    compute_shuffled_index, decrease_balance, get_active_validator_indices,
+    get_beacon_proposer_index, get_block_root, get_block_root_at_slot, get_current_epoch,
+    get_eligible_validator_indices, get_previous_epoch, get_seed, get_total_active_balance,
+    get_total_balance, increase_balance, initiate_validator_exit, is_in_inactivity_leak,
+    sync::SyncCommittee, AttestationData, BeaconState,
+};
+use ssz_rs::Vector;
 use std::collections::HashSet;
 
 pub fn add_flag(flags: ParticipationFlags, flag_index: u8) -> ParticipationFlags {
@@ -56,13 +58,17 @@ pub fn get_next_sync_committee_indices<
     let active_validator_count = active_validator_indices.len();
     let seed = get_seed(state, epoch, DomainType::SyncCommittee, context);
     let i = 0;
-    // @dev used `Vec` instead of `List`, which requires size to be known upon init
-    let mut sync_committee_indices = Vec::<ValidatorIndex>::new();    
+    let mut sync_committee_indices = Vec::<ValidatorIndex>::new();
     // @dev need to validate this is correct for `hash`
     let mut hash_input = [0u8; 40];
     hash_input[..32].copy_from_slice(seed.as_ref());
     while sync_committee_indices.len() < context.sync_committee_size {
-        let shuffled_index = compute_shuffled_index(i % active_validator_count, active_validator_count, &seed, context)?;
+        let shuffled_index = compute_shuffled_index(
+            i % active_validator_count,
+            active_validator_count,
+            &seed,
+            context,
+        )?;
         let candidate_index = active_validator_indices[shuffled_index];
 
         let i_bytes: [u8; 8] = (i / 32).to_le_bytes();
@@ -115,15 +121,17 @@ pub fn get_next_sync_committee<
         .collect::<Vec<_>>();
     // @dev not sure if the impl of `eth_aggregate_pubkeys` is needed or correct
     // also, not sure if `expect` is desired here
-    let aggregate_public_key = eth_aggregate_pubkeys(&public_keys).expect("validator public_keys should be aggregated into aggregate_public_key");
-    
+    let aggregate_public_key = eth_aggregate_pubkeys(&public_keys)
+        .expect("validator public_keys should be aggregated into aggregate_public_key");
+
     // @dev ran into trouble here so just cloned() the validator's public_key
     // couldn't figure out how to properly coerce/convert `public_keys` Vec to ssz_rs::Vector
-    let pks_vector = Vector::<_, SYNC_COMMITTEE_SIZE>::from_iter(public_keys.iter().map(|&pk| pk.clone()));
+    let pks_vector =
+        Vector::<_, SYNC_COMMITTEE_SIZE>::from_iter(public_keys.iter().map(|&pk| pk.clone()));
 
     Ok(SyncCommittee::<SYNC_COMMITTEE_SIZE> {
         public_keys: pks_vector,
-        aggregate_public_key
+        aggregate_public_key,
     })
 }
 
@@ -149,7 +157,10 @@ pub fn get_base_reward_per_increment<
     >,
     context: &Context,
 ) -> Result<Gwei> {
-    Ok(context.effective_balance_increment * context.base_reward_factor / get_total_active_balance(state, context)?.integer_sqrt())
+    Ok(
+        context.effective_balance_increment * context.base_reward_factor
+            / get_total_active_balance(state, context)?.integer_sqrt(),
+    )
 }
 
 pub fn get_base_reward<
@@ -175,8 +186,9 @@ pub fn get_base_reward<
     index: ValidatorIndex,
     context: &Context,
 ) -> Result<Gwei> {
-    // Return the base reward for the validator defined by `index` with respect to the current `state`
-    let increments = state.validators[index].effective_balance / context.effective_balance_increment;
+    // Return the base reward for the validator defined by ``index`` with respect to the current `state`
+    let increments =
+        state.validators[index].effective_balance / context.effective_balance_increment;
     Ok(increments * get_base_reward_per_increment(state, context)?)
 }
 
@@ -204,7 +216,7 @@ pub fn get_unslashed_participating_indices<
     epoch: Epoch,
     context: &Context,
 ) -> Result<HashSet<ValidatorIndex>> {
-    // Return the set of validator indices that are both active and unslashed for the given `flag_index` and `epoch`
+    // Return the set of validator indices that are both active and unslashed for the given ``flag_index`` and ``epoch``
     let previous_epoch = get_previous_epoch(state, context);
     let current_epoch = get_current_epoch(state, context);
     if previous_epoch != epoch && current_epoch != epoch {
@@ -215,25 +227,30 @@ pub fn get_unslashed_participating_indices<
         });
     }
 
-    let epoch_participation: &List<u8, VALIDATOR_REGISTRY_LIMIT>;
-    if epoch == current_epoch {
-        epoch_participation = &state.current_epoch_participation;
+    let epoch_participation = if epoch == current_epoch {
+        &state.current_epoch_participation
     } else {
-        epoch_participation = &state.previous_epoch_participation;
-    }
+        &state.previous_epoch_participation
+    };
     let active_validator_indices = get_active_validator_indices(state, epoch);
-    let participating_indices = active_validator_indices.iter().filter_map(|&i | {
-        if has_flag(epoch_participation[i], flag_index as u8) {
-            Some(i)
-        } else {
-            None
-        }
-    })
-    .collect::<Vec<_>>();
-
-    // TODO -- return set(filter(lambda index: not state.validators[index].slashed, participating_indices))
-
-    Ok(Default::default())
+    let participating_indices = active_validator_indices
+        .iter()
+        .filter_map(|&i| {
+            if has_flag(epoch_participation[i], flag_index as u8) {
+                Some(i)
+            } else {
+                None
+            }
+        })
+        .collect::<Vec<_>>();
+    let unslashed_particpating_indices = state
+        .validators
+        .iter()
+        .enumerate()
+        .filter_map(|(i, v)| if !v.slashed { Some(i) } else { None })
+        .filter(|i| participating_indices.contains(i))
+        .collect::<HashSet<_>>();
+    Ok(unslashed_particpating_indices)
 }
 
 pub fn get_attestation_participation_flag_indices<
@@ -246,7 +263,7 @@ pub fn get_attestation_participation_flag_indices<
     const MAX_VALIDATORS_PER_COMMITTEE: usize,
     const SYNC_COMMITTEE_SIZE: usize,
 >(
-    _state: &BeaconState<
+    state: &BeaconState<
         SLOTS_PER_HISTORICAL_ROOT,
         HISTORICAL_ROOTS_LIMIT,
         ETH1_DATA_VOTES_BOUND,
@@ -256,9 +273,45 @@ pub fn get_attestation_participation_flag_indices<
         MAX_VALIDATORS_PER_COMMITTEE,
         SYNC_COMMITTEE_SIZE,
     >,
-    _context: &Context,
-) -> Result<()> {
-    Ok(())
+    data: AttestationData,
+    inclusion_delay: u64,
+    context: &Context,
+) -> Result<Vec<usize>> {
+    // Return the flag indices that are satisfied by an attestation.
+    let justified_checkpoint = if data.target.epoch == get_current_epoch(state, context) {
+        &state.current_justified_checkpoint
+    } else {
+        &state.previous_justified_checkpoint
+    };
+
+    // Matching roots
+    let is_matching_source = data.source == *justified_checkpoint;
+    if !is_matching_source {
+        return Err(invalid_operation_error(InvalidOperation::Attestation(
+            InvalidAttestation::InvalidSource {
+                expected: justified_checkpoint.clone(),
+                source_checkpoint: data.source,
+                current: get_current_epoch(state, context),
+            },
+        )));
+    }
+    let is_matching_target = is_matching_source
+        && (data.target.root == *get_block_root(state, data.target.epoch, context)?);
+    let is_matching_head = is_matching_target
+        && (data.beacon_block_root == *get_block_root_at_slot(state, data.slot)?);
+
+    let mut participation_flag_indices = Vec::new();
+    if is_matching_source && inclusion_delay <= context.slots_per_epoch.integer_sqrt() {
+        participation_flag_indices.push(crate::altair::TIMELY_SOURCE_FLAG_INDEX);
+    }
+    if is_matching_target && inclusion_delay <= context.slots_per_epoch {
+        participation_flag_indices.push(crate::altair::TIMELY_TARGET_FLAG_INDEX);
+    }
+    if is_matching_head && inclusion_delay == context.min_attestation_inclusion_delay {
+        participation_flag_indices.push(crate::altair::TIMELY_HEAD_FLAG_INDEX);
+    }
+
+    Ok(participation_flag_indices)
 }
 
 pub fn get_flag_index_deltas<
@@ -271,7 +324,7 @@ pub fn get_flag_index_deltas<
     const MAX_VALIDATORS_PER_COMMITTEE: usize,
     const SYNC_COMMITTEE_SIZE: usize,
 >(
-    _state: &BeaconState<
+    state: &BeaconState<
         SLOTS_PER_HISTORICAL_ROOT,
         HISTORICAL_ROOTS_LIMIT,
         ETH1_DATA_VOTES_BOUND,
@@ -281,9 +334,36 @@ pub fn get_flag_index_deltas<
         MAX_VALIDATORS_PER_COMMITTEE,
         SYNC_COMMITTEE_SIZE,
     >,
-    _context: &Context,
-) -> Result<()> {
-    Ok(())
+    flag_index: usize,
+    context: &Context,
+) -> Result<(Vec<Gwei>, Vec<Gwei>)> {
+    // Return the deltas for a given ``flag_index`` by scanning through the participation flags.
+    let validator_count = state.validators.len();
+    let mut rewards = vec![0; validator_count];
+    let mut penalties = vec![0; validator_count];
+    let previous_epoch = get_previous_epoch(state, context);
+    let unslashed_participating_indices =
+        get_unslashed_participating_indices(state, flag_index, previous_epoch, context)?;
+    let weight = crate::altair::PARTICIPATION_FLAG_WEIGHTS[flag_index];
+    let unslashed_participating_balance =
+        get_total_balance(state, &unslashed_participating_indices, context)?;
+    let unslashed_participating_increments =
+        unslashed_participating_balance / context.effective_balance_increment;
+    let active_increments =
+        get_total_active_balance(state, context)? / context.effective_balance_increment;
+    for index in get_eligible_validator_indices(state, context) {
+        let base_reward = get_base_reward(state, index, context)?;
+        if unslashed_participating_indices.contains(&index) {
+            if !is_in_inactivity_leak(state, context) {
+                let reward_numerator = base_reward * weight * unslashed_participating_increments;
+                rewards[index] +=
+                    reward_numerator / (active_increments * crate::altair::WEIGHT_DENOMINATOR);
+            } else if flag_index != crate::altair::TIMELY_HEAD_FLAG_INDEX {
+                penalties[index] += base_reward * weight / crate::altair::WEIGHT_DENOMINATOR;
+            }
+        }
+    }
+    Ok((rewards, penalties))
 }
 
 pub fn get_inactivity_penalty_deltas<
