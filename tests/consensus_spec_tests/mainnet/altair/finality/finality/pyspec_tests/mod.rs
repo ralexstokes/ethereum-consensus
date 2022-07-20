2339f46ae2a9a25168b53ad000d8465979c05680
Alex Stokes <r.alex.stokes@gmail.com>
2022-07-20 15:20:07 -0600

    gen tests

--- tests/consensus_spec_tests/mainnet/altair/finality/finality/pyspec_tests/mod.rs
+++ tests/consensus_spec_tests/mainnet/altair/finality/finality/pyspec_tests/mod.rs
@@ -3,46 +3,72 @@
 
 use crate::spec_test_runners::finality::FinalityTestCase;
 use crate::test_utils::TestCase;
+use ethereum_consensus::altair::mainnet as spec;
 
 #[test]
 fn test_finality_no_updates_at_genesis() {
-    let  test_case = FinalityTestCase::from("consensus-spec-tests/tests/mainnet/altair/finality/finality/pyspec_tests/finality_no_updates_at_genesis");
+    let mut test_case = FinalityTestCase::<spec::BeaconState, spec::SignedBeaconBlock>::from("consensus-spec-tests/tests/mainnet/altair/finality/finality/pyspec_tests/finality_no_updates_at_genesis");
 
-    test_case.execute();
+    test_case.execute(|state, blocks, validation, context| {
+        for block in blocks.iter_mut() {
+            spec::state_transition(state, block, validation, context)?;
+        }
+        Ok(())
+    });
 }
 
 #[test]
 fn test_finality_rule_1() {
-    let test_case = FinalityTestCase::from(
+    let mut test_case = FinalityTestCase::<spec::BeaconState, spec::SignedBeaconBlock>::from(
         "consensus-spec-tests/tests/mainnet/altair/finality/finality/pyspec_tests/finality_rule_1",
     );
 
-    test_case.execute();
+    test_case.execute(|state, blocks, validation, context| {
+        for block in blocks.iter_mut() {
+            spec::state_transition(state, block, validation, context)?;
+        }
+        Ok(())
+    });
 }
 
 #[test]
 fn test_finality_rule_2() {
-    let test_case = FinalityTestCase::from(
+    let mut test_case = FinalityTestCase::<spec::BeaconState, spec::SignedBeaconBlock>::from(
         "consensus-spec-tests/tests/mainnet/altair/finality/finality/pyspec_tests/finality_rule_2",
     );
 
-    test_case.execute();
+    test_case.execute(|state, blocks, validation, context| {
+        for block in blocks.iter_mut() {
+            spec::state_transition(state, block, validation, context)?;
+        }
+        Ok(())
+    });
 }
 
 #[test]
 fn test_finality_rule_3() {
-    let test_case = FinalityTestCase::from(
+    let mut test_case = FinalityTestCase::<spec::BeaconState, spec::SignedBeaconBlock>::from(
         "consensus-spec-tests/tests/mainnet/altair/finality/finality/pyspec_tests/finality_rule_3",
     );
 
-    test_case.execute();
+    test_case.execute(|state, blocks, validation, context| {
+        for block in blocks.iter_mut() {
+            spec::state_transition(state, block, validation, context)?;
+        }
+        Ok(())
+    });
 }
 
 #[test]
 fn test_finality_rule_4() {
-    let test_case = FinalityTestCase::from(
+    let mut test_case = FinalityTestCase::<spec::BeaconState, spec::SignedBeaconBlock>::from(
         "consensus-spec-tests/tests/mainnet/altair/finality/finality/pyspec_tests/finality_rule_4",
     );
 
-    test_case.execute();
+    test_case.execute(|state, blocks, validation, context| {
+        for block in blocks.iter_mut() {
+            spec::state_transition(state, block, validation, context)?;
+        }
+        Ok(())
+    });
 }
