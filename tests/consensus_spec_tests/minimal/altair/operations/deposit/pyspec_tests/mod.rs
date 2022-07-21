d6c84b9b4adf8ab5da0178f4e1c9084654f177b9
Alex Stokes <r.alex.stokes@gmail.com>
2022-07-21 17:08:59 -0600

    test gen

--- tests/consensus_spec_tests/minimal/altair/operations/deposit/pyspec_tests/mod.rs
+++ tests/consensus_spec_tests/minimal/altair/operations/deposit/pyspec_tests/mod.rs
@@ -2,98 +2,100 @@
 // This file was generated with `gen-tests`. Do NOT edit manually.
 
 use crate::spec_test_runners::operations::DepositTestCase;
+use ethereum_consensus::altair::minimal as spec;
+use ssz_rs::prelude::*;
 
 #[test]
 fn test_bad_merkle_proof() {
-    let  test_case = DepositTestCase::from("consensus-spec-tests/tests/minimal/altair/operations/deposit/pyspec_tests/bad_merkle_proof");
+    let mut test_case = DepositTestCase::<spec::BeaconState, spec::Deposit>::from("consensus-spec-tests/tests/minimal/altair/operations/deposit/pyspec_tests/bad_merkle_proof");
 
-    test_case.execute();
+    test_case.execute(|state, operation, context| spec::process_deposit(state, operation, context));
 }
 
 #[test]
 fn test_invalid_sig_new_deposit() {
-    let  test_case = DepositTestCase::from("consensus-spec-tests/tests/minimal/altair/operations/deposit/pyspec_tests/invalid_sig_new_deposit");
+    let mut test_case = DepositTestCase::<spec::BeaconState, spec::Deposit>::from("consensus-spec-tests/tests/minimal/altair/operations/deposit/pyspec_tests/invalid_sig_new_deposit");
 
-    test_case.execute();
+    test_case.execute(|state, operation, context| spec::process_deposit(state, operation, context));
 }
 
 #[test]
 fn test_invalid_sig_other_version() {
-    let  test_case = DepositTestCase::from("consensus-spec-tests/tests/minimal/altair/operations/deposit/pyspec_tests/invalid_sig_other_version");
+    let mut test_case = DepositTestCase::<spec::BeaconState, spec::Deposit>::from("consensus-spec-tests/tests/minimal/altair/operations/deposit/pyspec_tests/invalid_sig_other_version");
 
-    test_case.execute();
+    test_case.execute(|state, operation, context| spec::process_deposit(state, operation, context));
 }
 
 #[test]
 fn test_invalid_sig_top_up() {
-    let  test_case = DepositTestCase::from("consensus-spec-tests/tests/minimal/altair/operations/deposit/pyspec_tests/invalid_sig_top_up");
+    let mut test_case = DepositTestCase::<spec::BeaconState, spec::Deposit>::from("consensus-spec-tests/tests/minimal/altair/operations/deposit/pyspec_tests/invalid_sig_top_up");
 
-    test_case.execute();
+    test_case.execute(|state, operation, context| spec::process_deposit(state, operation, context));
 }
 
 #[test]
 fn test_invalid_withdrawal_credentials_top_up() {
-    let  test_case = DepositTestCase::from("consensus-spec-tests/tests/minimal/altair/operations/deposit/pyspec_tests/invalid_withdrawal_credentials_top_up");
+    let mut test_case = DepositTestCase::<spec::BeaconState, spec::Deposit>::from("consensus-spec-tests/tests/minimal/altair/operations/deposit/pyspec_tests/invalid_withdrawal_credentials_top_up");
 
-    test_case.execute();
+    test_case.execute(|state, operation, context| spec::process_deposit(state, operation, context));
 }
 
 #[test]
 fn test_new_deposit_eth_1_withdrawal_credentials() {
-    let  test_case = DepositTestCase::from("consensus-spec-tests/tests/minimal/altair/operations/deposit/pyspec_tests/new_deposit_eth1_withdrawal_credentials");
+    let mut test_case = DepositTestCase::<spec::BeaconState, spec::Deposit>::from("consensus-spec-tests/tests/minimal/altair/operations/deposit/pyspec_tests/new_deposit_eth1_withdrawal_credentials");
 
-    test_case.execute();
+    test_case.execute(|state, operation, context| spec::process_deposit(state, operation, context));
 }
 
 #[test]
 fn test_new_deposit_max() {
-    let test_case = DepositTestCase::from(
+    let mut test_case = DepositTestCase::<spec::BeaconState, spec::Deposit>::from(
         "consensus-spec-tests/tests/minimal/altair/operations/deposit/pyspec_tests/new_deposit_max",
     );
 
-    test_case.execute();
+    test_case.execute(|state, operation, context| spec::process_deposit(state, operation, context));
 }
 
 #[test]
 fn test_new_deposit_non_versioned_withdrawal_credentials() {
-    let  test_case = DepositTestCase::from("consensus-spec-tests/tests/minimal/altair/operations/deposit/pyspec_tests/new_deposit_non_versioned_withdrawal_credentials");
+    let mut test_case = DepositTestCase::<spec::BeaconState, spec::Deposit>::from("consensus-spec-tests/tests/minimal/altair/operations/deposit/pyspec_tests/new_deposit_non_versioned_withdrawal_credentials");
 
-    test_case.execute();
+    test_case.execute(|state, operation, context| spec::process_deposit(state, operation, context));
 }
 
 #[test]
 fn test_new_deposit_over_max() {
-    let  test_case = DepositTestCase::from("consensus-spec-tests/tests/minimal/altair/operations/deposit/pyspec_tests/new_deposit_over_max");
+    let mut test_case = DepositTestCase::<spec::BeaconState, spec::Deposit>::from("consensus-spec-tests/tests/minimal/altair/operations/deposit/pyspec_tests/new_deposit_over_max");
 
-    test_case.execute();
+    test_case.execute(|state, operation, context| spec::process_deposit(state, operation, context));
 }
 
 #[test]
 fn test_new_deposit_under_max() {
-    let  test_case = DepositTestCase::from("consensus-spec-tests/tests/minimal/altair/operations/deposit/pyspec_tests/new_deposit_under_max");
+    let mut test_case = DepositTestCase::<spec::BeaconState, spec::Deposit>::from("consensus-spec-tests/tests/minimal/altair/operations/deposit/pyspec_tests/new_deposit_under_max");
 
-    test_case.execute();
+    test_case.execute(|state, operation, context| spec::process_deposit(state, operation, context));
 }
 
 #[test]
 fn test_success_top_up() {
-    let test_case = DepositTestCase::from(
+    let mut test_case = DepositTestCase::<spec::BeaconState, spec::Deposit>::from(
         "consensus-spec-tests/tests/minimal/altair/operations/deposit/pyspec_tests/success_top_up",
     );
 
-    test_case.execute();
+    test_case.execute(|state, operation, context| spec::process_deposit(state, operation, context));
 }
 
 #[test]
 fn test_valid_sig_but_forked_state() {
-    let  test_case = DepositTestCase::from("consensus-spec-tests/tests/minimal/altair/operations/deposit/pyspec_tests/valid_sig_but_forked_state");
+    let mut test_case = DepositTestCase::<spec::BeaconState, spec::Deposit>::from("consensus-spec-tests/tests/minimal/altair/operations/deposit/pyspec_tests/valid_sig_but_forked_state");
 
-    test_case.execute();
+    test_case.execute(|state, operation, context| spec::process_deposit(state, operation, context));
 }
 
 #[test]
 fn test_wrong_deposit_for_deposit_count() {
-    let  test_case = DepositTestCase::from("consensus-spec-tests/tests/minimal/altair/operations/deposit/pyspec_tests/wrong_deposit_for_deposit_count");
+    let mut test_case = DepositTestCase::<spec::BeaconState, spec::Deposit>::from("consensus-spec-tests/tests/minimal/altair/operations/deposit/pyspec_tests/wrong_deposit_for_deposit_count");
 
-    test_case.execute();
+    test_case.execute(|state, operation, context| spec::process_deposit(state, operation, context));
 }
--- tests/consensus_spec_tests/minimal/altair/operations/proposer_slashing/pyspec_tests/mod.rs
+++ tests/consensus_spec_tests/minimal/altair/operations/proposer_slashing/pyspec_tests/mod.rs
@@ -2,108 +2,140 @@
 // This file was generated with `gen-tests`. Do NOT edit manually.
 
 use crate::spec_test_runners::operations::ProposerSlashingTestCase;
+use ethereum_consensus::altair::minimal as spec;
+use ssz_rs::prelude::*;
 
 #[test]
 fn test_epochs_are_different() {
-    let  test_case = ProposerSlashingTestCase::from("consensus-spec-tests/tests/minimal/altair/operations/proposer_slashing/pyspec_tests/epochs_are_different");
+    let mut test_case = ProposerSlashingTestCase::<spec::BeaconState, spec::ProposerSlashing>::from("consensus-spec-tests/tests/minimal/altair/operations/proposer_slashing/pyspec_tests/epochs_are_different");
 
-    test_case.execute();
+    test_case.execute(|state, operation, context| {
+        spec::process_proposer_slashing(state, operation, context)
+    });
 }
 
 #[test]
 fn test_headers_are_same_sigs_are_different() {
-    let  test_case = ProposerSlashingTestCase::from("consensus-spec-tests/tests/minimal/altair/operations/proposer_slashing/pyspec_tests/headers_are_same_sigs_are_different");
+    let mut test_case = ProposerSlashingTestCase::<spec::BeaconState, spec::ProposerSlashing>::from("consensus-spec-tests/tests/minimal/altair/operations/proposer_slashing/pyspec_tests/headers_are_same_sigs_are_different");
 
-    test_case.execute();
+    test_case.execute(|state, operation, context| {
+        spec::process_proposer_slashing(state, operation, context)
+    });
 }
 
 #[test]
 fn test_headers_are_same_sigs_are_same() {
-    let  test_case = ProposerSlashingTestCase::from("consensus-spec-tests/tests/minimal/altair/operations/proposer_slashing/pyspec_tests/headers_are_same_sigs_are_same");
+    let mut test_case = ProposerSlashingTestCase::<spec::BeaconState, spec::ProposerSlashing>::from("consensus-spec-tests/tests/minimal/altair/operations/proposer_slashing/pyspec_tests/headers_are_same_sigs_are_same");
 
-    test_case.execute();
+    test_case.execute(|state, operation, context| {
+        spec::process_proposer_slashing(state, operation, context)
+    });
 }
 
 #[test]
 fn test_invalid_different_proposer_indices() {
-    let  test_case = ProposerSlashingTestCase::from("consensus-spec-tests/tests/minimal/altair/operations/proposer_slashing/pyspec_tests/invalid_different_proposer_indices");
+    let mut test_case = ProposerSlashingTestCase::<spec::BeaconState, spec::ProposerSlashing>::from("consensus-spec-tests/tests/minimal/altair/operations/proposer_slashing/pyspec_tests/invalid_different_proposer_indices");
 
-    test_case.execute();
+    test_case.execute(|state, operation, context| {
+        spec::process_proposer_slashing(state, operation, context)
+    });
 }
 
 #[test]
 fn test_invalid_proposer_index() {
-    let  test_case = ProposerSlashingTestCase::from("consensus-spec-tests/tests/minimal/altair/operations/proposer_slashing/pyspec_tests/invalid_proposer_index");
+    let mut test_case = ProposerSlashingTestCase::<spec::BeaconState, spec::ProposerSlashing>::from("consensus-spec-tests/tests/minimal/altair/operations/proposer_slashing/pyspec_tests/invalid_proposer_index");
 
-    test_case.execute();
+    test_case.execute(|state, operation, context| {
+        spec::process_proposer_slashing(state, operation, context)
+    });
 }
 
 #[test]
 fn test_invalid_sig_1() {
-    let  test_case = ProposerSlashingTestCase::from("consensus-spec-tests/tests/minimal/altair/operations/proposer_slashing/pyspec_tests/invalid_sig_1");
+    let mut test_case = ProposerSlashingTestCase::<spec::BeaconState, spec::ProposerSlashing>::from("consensus-spec-tests/tests/minimal/altair/operations/proposer_slashing/pyspec_tests/invalid_sig_1");
 
-    test_case.execute();
+    test_case.execute(|state, operation, context| {
+        spec::process_proposer_slashing(state, operation, context)
+    });
 }
 
 #[test]
 fn test_invalid_sig_1_and_2() {
-    let  test_case = ProposerSlashingTestCase::from("consensus-spec-tests/tests/minimal/altair/operations/proposer_slashing/pyspec_tests/invalid_sig_1_and_2");
+    let mut test_case = ProposerSlashingTestCase::<spec::BeaconState, spec::ProposerSlashing>::from("consensus-spec-tests/tests/minimal/altair/operations/proposer_slashing/pyspec_tests/invalid_sig_1_and_2");
 
-    test_case.execute();
+    test_case.execute(|state, operation, context| {
+        spec::process_proposer_slashing(state, operation, context)
+    });
 }
 
 #[test]
 fn test_invalid_sig_1_and_2_swap() {
-    let  test_case = ProposerSlashingTestCase::from("consensus-spec-tests/tests/minimal/altair/operations/proposer_slashing/pyspec_tests/invalid_sig_1_and_2_swap");
+    let mut test_case = ProposerSlashingTestCase::<spec::BeaconState, spec::ProposerSlashing>::from("consensus-spec-tests/tests/minimal/altair/operations/proposer_slashing/pyspec_tests/invalid_sig_1_and_2_swap");
 
-    test_case.execute();
+    test_case.execute(|state, operation, context| {
+        spec::process_proposer_slashing(state, operation, context)
+    });
 }
 
 #[test]
 fn test_invalid_sig_2() {
-    let  test_case = ProposerSlashingTestCase::from("consensus-spec-tests/tests/minimal/altair/operations/proposer_slashing/pyspec_tests/invalid_sig_2");
+    let mut test_case = ProposerSlashingTestCase::<spec::BeaconState, spec::ProposerSlashing>::from("consensus-spec-tests/tests/minimal/altair/operations/proposer_slashing/pyspec_tests/invalid_sig_2");
 
-    test_case.execute();
+    test_case.execute(|state, operation, context| {
+        spec::process_proposer_slashing(state, operation, context)
+    });
 }
 
 #[test]
 fn test_proposer_is_not_activated() {
-    let  test_case = ProposerSlashingTestCase::from("consensus-spec-tests/tests/minimal/altair/operations/proposer_slashing/pyspec_tests/proposer_is_not_activated");
+    let mut test_case = ProposerSlashingTestCase::<spec::BeaconState, spec::ProposerSlashing>::from("consensus-spec-tests/tests/minimal/altair/operations/proposer_slashing/pyspec_tests/proposer_is_not_activated");
 
-    test_case.execute();
+    test_case.execute(|state, operation, context| {
+        spec::process_proposer_slashing(state, operation, context)
+    });
 }
 
 #[test]
 fn test_proposer_is_slashed() {
-    let  test_case = ProposerSlashingTestCase::from("consensus-spec-tests/tests/minimal/altair/operations/proposer_slashing/pyspec_tests/proposer_is_slashed");
+    let mut test_case = ProposerSlashingTestCase::<spec::BeaconState, spec::ProposerSlashing>::from("consensus-spec-tests/tests/minimal/altair/operations/proposer_slashing/pyspec_tests/proposer_is_slashed");
 
-    test_case.execute();
+    test_case.execute(|state, operation, context| {
+        spec::process_proposer_slashing(state, operation, context)
+    });
 }
 
 #[test]
 fn test_proposer_is_withdrawn() {
-    let  test_case = ProposerSlashingTestCase::from("consensus-spec-tests/tests/minimal/altair/operations/proposer_slashing/pyspec_tests/proposer_is_withdrawn");
+    let mut test_case = ProposerSlashingTestCase::<spec::BeaconState, spec::ProposerSlashing>::from("consensus-spec-tests/tests/minimal/altair/operations/proposer_slashing/pyspec_tests/proposer_is_withdrawn");
 
-    test_case.execute();
+    test_case.execute(|state, operation, context| {
+        spec::process_proposer_slashing(state, operation, context)
+    });
 }
 
 #[test]
 fn test_success() {
-    let  test_case = ProposerSlashingTestCase::from("consensus-spec-tests/tests/minimal/altair/operations/proposer_slashing/pyspec_tests/success");
+    let mut test_case = ProposerSlashingTestCase::<spec::BeaconState, spec::ProposerSlashing>::from("consensus-spec-tests/tests/minimal/altair/operations/proposer_slashing/pyspec_tests/success");
 
-    test_case.execute();
+    test_case.execute(|state, operation, context| {
+        spec::process_proposer_slashing(state, operation, context)
+    });
 }
 
 #[test]
 fn test_success_block_header_from_future() {
-    let  test_case = ProposerSlashingTestCase::from("consensus-spec-tests/tests/minimal/altair/operations/proposer_slashing/pyspec_tests/success_block_header_from_future");
+    let mut test_case = ProposerSlashingTestCase::<spec::BeaconState, spec::ProposerSlashing>::from("consensus-spec-tests/tests/minimal/altair/operations/proposer_slashing/pyspec_tests/success_block_header_from_future");
 
-    test_case.execute();
+    test_case.execute(|state, operation, context| {
+        spec::process_proposer_slashing(state, operation, context)
+    });
 }
 
 #[test]
 fn test_success_slashed_and_proposer_index_the_same() {
-    let  test_case = ProposerSlashingTestCase::from("consensus-spec-tests/tests/minimal/altair/operations/proposer_slashing/pyspec_tests/success_slashed_and_proposer_index_the_same");
+    let mut test_case = ProposerSlashingTestCase::<spec::BeaconState, spec::ProposerSlashing>::from("consensus-spec-tests/tests/minimal/altair/operations/proposer_slashing/pyspec_tests/success_slashed_and_proposer_index_the_same");
 
-    test_case.execute();
+    test_case.execute(|state, operation, context| {
+        spec::process_proposer_slashing(state, operation, context)
+    });
 }
