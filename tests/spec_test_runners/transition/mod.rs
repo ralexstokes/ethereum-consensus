f5b786dc4bcd8c16e1b26f9bbb5a8028dabbab3b
Alex Stokes <r.alex.stokes@gmail.com>
2022-07-22 13:58:54 -0600

    impl `transition` test handler

--- tests/spec_test_runners/transition/mod.rs
+++ tests/spec_test_runners/transition/mod.rs
@@ -1,11 +1,87 @@
-pub struct CoreTestCase;
+use crate::test_utils::{load_snappy_ssz, load_yaml, Config};
+use ethereum_consensus::primitives::Epoch;
+use ethereum_consensus::state_transition::{Context, Executor, Result, Validation};
+use serde::Deserialize;
+use std::fmt;
 
-impl CoreTestCase {
+pub struct CoreTestCase<S> {
+    test_case_path: String,
+    post: S,
+    meta: Meta,
+    config: Config,
+}
+
+#[derive(Deserialize)]
+struct Meta {
+    post_fork: String,
+    fork_epoch: Epoch,
+    #[serde(default)]
+    fork_block: usize,
+    blocks_count: usize,
+}
+
+impl<S> CoreTestCase<S>
+where
+    S: fmt::Debug + ssz_rs::Deserialize + PartialEq<S>,
+{
     pub fn from(test_case_path: &str) -> Self {
-        Self
+        let path = test_case_path.to_string() + "/post.ssz_snappy";
+        let post: S = load_snappy_ssz(&path).unwrap();
+
+        let path = test_case_path.to_string() + "/meta.yaml";
+        let meta: Meta = load_yaml(&path);
+
+        let config = if test_case_path.contains("minimal") {
+            Config::Minimal
+        } else {
+            Config::Mainnet
+        };
+
+        Self {
+            test_case_path: test_case_path.to_string(),
+            post,
+            meta,
+            config,
+        }
     }
 
-    pub fn execute(&self) {
-        unimplemented!();
+    pub fn execute<F, B>(&mut self, f: F)
+    where
+        F: FnOnce(S, Vec<B>, Context) -> Result<S>,
+        B: fmt::Debug + ssz_rs::Deserialize + PartialEq<B>,
+    {
+        let path = self.test_case_path.to_string() + "/pre.ssz_snappy";
+        let pre: S = load_snappy_ssz(&path).unwrap();
+
+        let blocks_count = self.meta.blocks_count;
+        let mut blocks = vec![];
+        for i in 0..blocks_count {
+            let path = format!(
+                "{}/blocks_{}.ssz_snappy",
+                self.test_case_path.to_string(),
+                i
+            );
+            let block: B = load_snappy_ssz(&path).unwrap();
+            blocks.push(block);
+        }
+
+        let mut context = match self.config {
+            Config::Minimal => Context::for_minimal(),
+            Config::Mainnet => Context::for_mainnet(),
+        };
+        match self.meta.post_fork.as_ref() {
+            "altair" => {
+                context.fork_schedule.altair = self.meta.fork_epoch;
+                context.fork_schedule.bellatrix = Epoch::MAX;
+            }
+            "bellatrix" => {
+                context.fork_schedule.altair = 0;
+                context.fork_schedule.bellatrix = self.meta.fork_epoch;
+            }
+            _ => todo!("support other forks"),
+        }
+
+        let post = f(pre, blocks, context).unwrap();
+        assert_eq!(&post, &self.post);
     }
 }
