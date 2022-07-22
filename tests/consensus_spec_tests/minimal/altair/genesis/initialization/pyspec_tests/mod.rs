cc68590d63dbb7dfa49c31b80ca4cdda2285b530
Alex Stokes <r.alex.stokes@gmail.com>
2022-07-22 17:37:08 -0600

    impl `initialization` handler of `genesis` runner for spec tests

--- tests/consensus_spec_tests/minimal/altair/genesis/initialization/pyspec_tests/mod.rs
+++ tests/consensus_spec_tests/minimal/altair/genesis/initialization/pyspec_tests/mod.rs
@@ -2,38 +2,130 @@
 // This file was generated with `gen-tests`. Do NOT edit manually.
 
 use crate::spec_test_runners::genesis::InitializationTestCase;
+use ethereum_consensus::altair::minimal as spec;
+use ssz_rs::prelude::*;
 
 #[test]
 fn test_initialize_beacon_state_from_eth_1() {
-    let  test_case = InitializationTestCase::from("consensus-spec-tests/tests/minimal/altair/genesis/initialization/pyspec_tests/initialize_beacon_state_from_eth1");
+    let mut test_case = InitializationTestCase::<spec::BeaconState, spec::Deposit, usize>::from("consensus-spec-tests/tests/minimal/altair/genesis/initialization/pyspec_tests/initialize_beacon_state_from_eth1");
 
-    test_case.execute();
+    test_case.execute(
+        |eth1_block_hash, eth1_timestamp, deposits, _execution_payload_header, context| {
+            spec::initialize_beacon_state_from_eth1::<
+                { spec::SLOTS_PER_HISTORICAL_ROOT },
+                { spec::HISTORICAL_ROOTS_LIMIT },
+                { spec::ETH1_DATA_VOTES_BOUND },
+                { spec::VALIDATOR_REGISTRY_LIMIT },
+                { spec::EPOCHS_PER_HISTORICAL_VECTOR },
+                { spec::EPOCHS_PER_SLASHINGS_VECTOR },
+                { spec::MAX_VALIDATORS_PER_COMMITTEE },
+                { spec::SYNC_COMMITTEE_SIZE },
+                { spec::MAX_PROPOSER_SLASHINGS },
+                { spec::MAX_ATTESTER_SLASHINGS },
+                { spec::MAX_ATTESTATIONS },
+                { spec::MAX_DEPOSITS },
+                { spec::MAX_VOLUNTARY_EXITS },
+            >(eth1_block_hash, eth1_timestamp, deposits, context)
+        },
+    );
 }
 
 #[test]
 fn test_initialize_beacon_state_one_topup_activation() {
-    let  test_case = InitializationTestCase::from("consensus-spec-tests/tests/minimal/altair/genesis/initialization/pyspec_tests/initialize_beacon_state_one_topup_activation");
+    let mut test_case = InitializationTestCase::<spec::BeaconState, spec::Deposit, usize>::from("consensus-spec-tests/tests/minimal/altair/genesis/initialization/pyspec_tests/initialize_beacon_state_one_topup_activation");
 
-    test_case.execute();
+    test_case.execute(
+        |eth1_block_hash, eth1_timestamp, deposits, _execution_payload_header, context| {
+            spec::initialize_beacon_state_from_eth1::<
+                { spec::SLOTS_PER_HISTORICAL_ROOT },
+                { spec::HISTORICAL_ROOTS_LIMIT },
+                { spec::ETH1_DATA_VOTES_BOUND },
+                { spec::VALIDATOR_REGISTRY_LIMIT },
+                { spec::EPOCHS_PER_HISTORICAL_VECTOR },
+                { spec::EPOCHS_PER_SLASHINGS_VECTOR },
+                { spec::MAX_VALIDATORS_PER_COMMITTEE },
+                { spec::SYNC_COMMITTEE_SIZE },
+                { spec::MAX_PROPOSER_SLASHINGS },
+                { spec::MAX_ATTESTER_SLASHINGS },
+                { spec::MAX_ATTESTATIONS },
+                { spec::MAX_DEPOSITS },
+                { spec::MAX_VOLUNTARY_EXITS },
+            >(eth1_block_hash, eth1_timestamp, deposits, context)
+        },
+    );
 }
 
 #[test]
 fn test_initialize_beacon_state_random_invalid_genesis() {
-    let  test_case = InitializationTestCase::from("consensus-spec-tests/tests/minimal/altair/genesis/initialization/pyspec_tests/initialize_beacon_state_random_invalid_genesis");
+    let mut test_case = InitializationTestCase::<spec::BeaconState, spec::Deposit, usize>::from("consensus-spec-tests/tests/minimal/altair/genesis/initialization/pyspec_tests/initialize_beacon_state_random_invalid_genesis");
 
-    test_case.execute();
+    test_case.execute(
+        |eth1_block_hash, eth1_timestamp, deposits, _execution_payload_header, context| {
+            spec::initialize_beacon_state_from_eth1::<
+                { spec::SLOTS_PER_HISTORICAL_ROOT },
+                { spec::HISTORICAL_ROOTS_LIMIT },
+                { spec::ETH1_DATA_VOTES_BOUND },
+                { spec::VALIDATOR_REGISTRY_LIMIT },
+                { spec::EPOCHS_PER_HISTORICAL_VECTOR },
+                { spec::EPOCHS_PER_SLASHINGS_VECTOR },
+                { spec::MAX_VALIDATORS_PER_COMMITTEE },
+                { spec::SYNC_COMMITTEE_SIZE },
+                { spec::MAX_PROPOSER_SLASHINGS },
+                { spec::MAX_ATTESTER_SLASHINGS },
+                { spec::MAX_ATTESTATIONS },
+                { spec::MAX_DEPOSITS },
+                { spec::MAX_VOLUNTARY_EXITS },
+            >(eth1_block_hash, eth1_timestamp, deposits, context)
+        },
+    );
 }
 
 #[test]
 fn test_initialize_beacon_state_random_valid_genesis() {
-    let  test_case = InitializationTestCase::from("consensus-spec-tests/tests/minimal/altair/genesis/initialization/pyspec_tests/initialize_beacon_state_random_valid_genesis");
+    let mut test_case = InitializationTestCase::<spec::BeaconState, spec::Deposit, usize>::from("consensus-spec-tests/tests/minimal/altair/genesis/initialization/pyspec_tests/initialize_beacon_state_random_valid_genesis");
 
-    test_case.execute();
+    test_case.execute(
+        |eth1_block_hash, eth1_timestamp, deposits, _execution_payload_header, context| {
+            spec::initialize_beacon_state_from_eth1::<
+                { spec::SLOTS_PER_HISTORICAL_ROOT },
+                { spec::HISTORICAL_ROOTS_LIMIT },
+                { spec::ETH1_DATA_VOTES_BOUND },
+                { spec::VALIDATOR_REGISTRY_LIMIT },
+                { spec::EPOCHS_PER_HISTORICAL_VECTOR },
+                { spec::EPOCHS_PER_SLASHINGS_VECTOR },
+                { spec::MAX_VALIDATORS_PER_COMMITTEE },
+                { spec::SYNC_COMMITTEE_SIZE },
+                { spec::MAX_PROPOSER_SLASHINGS },
+                { spec::MAX_ATTESTER_SLASHINGS },
+                { spec::MAX_ATTESTATIONS },
+                { spec::MAX_DEPOSITS },
+                { spec::MAX_VOLUNTARY_EXITS },
+            >(eth1_block_hash, eth1_timestamp, deposits, context)
+        },
+    );
 }
 
 #[test]
 fn test_initialize_beacon_state_some_small_balances() {
-    let  test_case = InitializationTestCase::from("consensus-spec-tests/tests/minimal/altair/genesis/initialization/pyspec_tests/initialize_beacon_state_some_small_balances");
+    let mut test_case = InitializationTestCase::<spec::BeaconState, spec::Deposit, usize>::from("consensus-spec-tests/tests/minimal/altair/genesis/initialization/pyspec_tests/initialize_beacon_state_some_small_balances");
 
-    test_case.execute();
+    test_case.execute(
+        |eth1_block_hash, eth1_timestamp, deposits, _execution_payload_header, context| {
+            spec::initialize_beacon_state_from_eth1::<
+                { spec::SLOTS_PER_HISTORICAL_ROOT },
+                { spec::HISTORICAL_ROOTS_LIMIT },
+                { spec::ETH1_DATA_VOTES_BOUND },
+                { spec::VALIDATOR_REGISTRY_LIMIT },
+                { spec::EPOCHS_PER_HISTORICAL_VECTOR },
+                { spec::EPOCHS_PER_SLASHINGS_VECTOR },
+                { spec::MAX_VALIDATORS_PER_COMMITTEE },
+                { spec::SYNC_COMMITTEE_SIZE },
+                { spec::MAX_PROPOSER_SLASHINGS },
+                { spec::MAX_ATTESTER_SLASHINGS },
+                { spec::MAX_ATTESTATIONS },
+                { spec::MAX_DEPOSITS },
+                { spec::MAX_VOLUNTARY_EXITS },
+            >(eth1_block_hash, eth1_timestamp, deposits, context)
+        },
+    );
 }
--- tests/spec_test_runners/genesis/mod.rs
+++ tests/spec_test_runners/genesis/mod.rs
@@ -1,16 +1,92 @@
 use crate::test_utils::{load_snappy_ssz, load_yaml, Config};
-use ethereum_consensus::state_transition::Context;
+use ethereum_consensus::primitives::{Bytes32, Hash32};
+use ethereum_consensus::state_transition::{Context, Result};
 use serde::Deserialize;
+use std::fmt;
 
-pub struct InitializationTestCase;
+#[derive(Deserialize)]
+struct Eth1 {
+    eth1_block_hash: Bytes32,
+    eth1_timestamp: u64,
+}
 
-impl InitializationTestCase {
+#[derive(Deserialize)]
+struct Meta {
+    deposits_count: usize,
+    execution_payload_header: bool,
+}
+
+pub struct InitializationTestCase<S, D, H> {
+    eth1: Eth1,
+    deposits: Vec<D>,
+    execution_payload_header: Option<H>,
+    state: S,
+    config: Config,
+}
+
+impl<S, D, H> InitializationTestCase<S, D, H>
+where
+    S: fmt::Debug + ssz_rs::Deserialize + PartialEq<S>,
+    D: ssz_rs::Deserialize,
+    H: ssz_rs::Deserialize,
+{
     pub fn from(test_case_path: &str) -> Self {
-        Self
+        let path = test_case_path.to_string() + "/eth1.yaml";
+        let eth1: Eth1 = load_yaml(&path);
+
+        let path = test_case_path.to_string() + "/meta.yaml";
+        let meta: Meta = load_yaml(&path);
+
+        let mut deposits = vec![];
+        for i in 0..meta.deposits_count {
+            let path = format!("{}/deposits_{}.ssz_snappy", test_case_path, i);
+            let deposit: D = load_snappy_ssz(&path).unwrap();
+            deposits.push(deposit);
+        }
+
+        let execution_payload_header = if meta.execution_payload_header {
+            let path = test_case_path.to_string() + "/execution_payload_header.ssz_snappy";
+            let header: H = load_snappy_ssz(&path).unwrap();
+            Some(header)
+        } else {
+            None
+        };
+
+        let path = test_case_path.to_string() + "/state.ssz_snappy";
+        let state: S = load_snappy_ssz(&path).unwrap();
+
+        let config = if test_case_path.contains("minimal") {
+            Config::Minimal
+        } else {
+            Config::Mainnet
+        };
+
+        Self {
+            eth1,
+            deposits,
+            execution_payload_header,
+            state,
+            config,
+        }
     }
 
-    pub fn execute(&self) {
-        unimplemented!();
+    pub fn execute<F>(&mut self, f: F)
+    where
+        F: FnOnce(Hash32, u64, &mut [D], Option<&H>, &Context) -> Result<S>,
+    {
+        let context = match self.config {
+            Config::Minimal => Context::for_minimal(),
+            Config::Mainnet => Context::for_mainnet(),
+        };
+        let state = f(
+            self.eth1.eth1_block_hash.clone(),
+            self.eth1.eth1_timestamp,
+            &mut self.deposits,
+            self.execution_payload_header.as_ref(),
+            &context,
+        )
+        .unwrap();
+        assert_eq!(state, self.state);
     }
 }
 
