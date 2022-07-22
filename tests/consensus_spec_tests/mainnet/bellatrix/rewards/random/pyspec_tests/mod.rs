36f0652df16ec71925820c8a2e7bdb05e6efbd50
Alex Stokes <r.alex.stokes@gmail.com>
2022-07-22 08:17:46 -0600

    test gen

--- tests/consensus_spec_tests/mainnet/bellatrix/rewards/random/pyspec_tests/mod.rs
+++ tests/consensus_spec_tests/mainnet/bellatrix/rewards/random/pyspec_tests/mod.rs
@@ -2,83 +2,245 @@
 // This file was generated with `gen-tests`. Do NOT edit manually.
 
 use crate::spec_test_runners::rewards::RandomTestCase;
+use ethereum_consensus::bellatrix::mainnet as spec;
+use ssz_rs::prelude::*;
 
 #[test]
 fn test_full_random_0() {
-    let test_case = RandomTestCase::from(
+    let test_case = RandomTestCase::<spec::BeaconState>::from(
         "consensus-spec-tests/tests/mainnet/bellatrix/rewards/random/pyspec_tests/full_random_0",
     );
 
-    test_case.execute();
+    test_case.execute(|state, context| {
+        let flag_index = spec::TIMELY_SOURCE_FLAG_INDEX;
+        let source_deltas = spec::get_flag_index_deltas(state, flag_index, context).unwrap();
+        let flag_index = spec::TIMELY_TARGET_FLAG_INDEX;
+        let target_deltas = spec::get_flag_index_deltas(state, flag_index, context).unwrap();
+        let flag_index = spec::TIMELY_HEAD_FLAG_INDEX;
+        let head_deltas = spec::get_flag_index_deltas(state, flag_index, context).unwrap();
+        let inactivity_penalty_deltas =
+            spec::get_inactivity_penalty_deltas(state, context).unwrap();
+        (
+            source_deltas,
+            target_deltas,
+            head_deltas,
+            None,
+            inactivity_penalty_deltas,
+        )
+    });
 }
 
 #[test]
 fn test_full_random_1() {
-    let test_case = RandomTestCase::from(
+    let test_case = RandomTestCase::<spec::BeaconState>::from(
         "consensus-spec-tests/tests/mainnet/bellatrix/rewards/random/pyspec_tests/full_random_1",
     );
 
-    test_case.execute();
+    test_case.execute(|state, context| {
+        let flag_index = spec::TIMELY_SOURCE_FLAG_INDEX;
+        let source_deltas = spec::get_flag_index_deltas(state, flag_index, context).unwrap();
+        let flag_index = spec::TIMELY_TARGET_FLAG_INDEX;
+        let target_deltas = spec::get_flag_index_deltas(state, flag_index, context).unwrap();
+        let flag_index = spec::TIMELY_HEAD_FLAG_INDEX;
+        let head_deltas = spec::get_flag_index_deltas(state, flag_index, context).unwrap();
+        let inactivity_penalty_deltas =
+            spec::get_inactivity_penalty_deltas(state, context).unwrap();
+        (
+            source_deltas,
+            target_deltas,
+            head_deltas,
+            None,
+            inactivity_penalty_deltas,
+        )
+    });
 }
 
 #[test]
 fn test_full_random_2() {
-    let test_case = RandomTestCase::from(
+    let test_case = RandomTestCase::<spec::BeaconState>::from(
         "consensus-spec-tests/tests/mainnet/bellatrix/rewards/random/pyspec_tests/full_random_2",
     );
 
-    test_case.execute();
+    test_case.execute(|state, context| {
+        let flag_index = spec::TIMELY_SOURCE_FLAG_INDEX;
+        let source_deltas = spec::get_flag_index_deltas(state, flag_index, context).unwrap();
+        let flag_index = spec::TIMELY_TARGET_FLAG_INDEX;
+        let target_deltas = spec::get_flag_index_deltas(state, flag_index, context).unwrap();
+        let flag_index = spec::TIMELY_HEAD_FLAG_INDEX;
+        let head_deltas = spec::get_flag_index_deltas(state, flag_index, context).unwrap();
+        let inactivity_penalty_deltas =
+            spec::get_inactivity_penalty_deltas(state, context).unwrap();
+        (
+            source_deltas,
+            target_deltas,
+            head_deltas,
+            None,
+            inactivity_penalty_deltas,
+        )
+    });
 }
 
 #[test]
 fn test_full_random_3() {
-    let test_case = RandomTestCase::from(
+    let test_case = RandomTestCase::<spec::BeaconState>::from(
         "consensus-spec-tests/tests/mainnet/bellatrix/rewards/random/pyspec_tests/full_random_3",
     );
 
-    test_case.execute();
+    test_case.execute(|state, context| {
+        let flag_index = spec::TIMELY_SOURCE_FLAG_INDEX;
+        let source_deltas = spec::get_flag_index_deltas(state, flag_index, context).unwrap();
+        let flag_index = spec::TIMELY_TARGET_FLAG_INDEX;
+        let target_deltas = spec::get_flag_index_deltas(state, flag_index, context).unwrap();
+        let flag_index = spec::TIMELY_HEAD_FLAG_INDEX;
+        let head_deltas = spec::get_flag_index_deltas(state, flag_index, context).unwrap();
+        let inactivity_penalty_deltas =
+            spec::get_inactivity_penalty_deltas(state, context).unwrap();
+        (
+            source_deltas,
+            target_deltas,
+            head_deltas,
+            None,
+            inactivity_penalty_deltas,
+        )
+    });
 }
 
 #[test]
 fn test_full_random_4() {
-    let test_case = RandomTestCase::from(
+    let test_case = RandomTestCase::<spec::BeaconState>::from(
         "consensus-spec-tests/tests/mainnet/bellatrix/rewards/random/pyspec_tests/full_random_4",
     );
 
-    test_case.execute();
+    test_case.execute(|state, context| {
+        let flag_index = spec::TIMELY_SOURCE_FLAG_INDEX;
+        let source_deltas = spec::get_flag_index_deltas(state, flag_index, context).unwrap();
+        let flag_index = spec::TIMELY_TARGET_FLAG_INDEX;
+        let target_deltas = spec::get_flag_index_deltas(state, flag_index, context).unwrap();
+        let flag_index = spec::TIMELY_HEAD_FLAG_INDEX;
+        let head_deltas = spec::get_flag_index_deltas(state, flag_index, context).unwrap();
+        let inactivity_penalty_deltas =
+            spec::get_inactivity_penalty_deltas(state, context).unwrap();
+        (
+            source_deltas,
+            target_deltas,
+            head_deltas,
+            None,
+            inactivity_penalty_deltas,
+        )
+    });
 }
 
 #[test]
 fn test_full_random_low_balances_0() {
-    let  test_case = RandomTestCase::from("consensus-spec-tests/tests/mainnet/bellatrix/rewards/random/pyspec_tests/full_random_low_balances_0");
+    let  test_case = RandomTestCase::<spec::BeaconState>::from("consensus-spec-tests/tests/mainnet/bellatrix/rewards/random/pyspec_tests/full_random_low_balances_0");
 
-    test_case.execute();
+    test_case.execute(|state, context| {
+        let flag_index = spec::TIMELY_SOURCE_FLAG_INDEX;
+        let source_deltas = spec::get_flag_index_deltas(state, flag_index, context).unwrap();
+        let flag_index = spec::TIMELY_TARGET_FLAG_INDEX;
+        let target_deltas = spec::get_flag_index_deltas(state, flag_index, context).unwrap();
+        let flag_index = spec::TIMELY_HEAD_FLAG_INDEX;
+        let head_deltas = spec::get_flag_index_deltas(state, flag_index, context).unwrap();
+        let inactivity_penalty_deltas =
+            spec::get_inactivity_penalty_deltas(state, context).unwrap();
+        (
+            source_deltas,
+            target_deltas,
+            head_deltas,
+            None,
+            inactivity_penalty_deltas,
+        )
+    });
 }
 
 #[test]
 fn test_full_random_low_balances_1() {
-    let  test_case = RandomTestCase::from("consensus-spec-tests/tests/mainnet/bellatrix/rewards/random/pyspec_tests/full_random_low_balances_1");
+    let  test_case = RandomTestCase::<spec::BeaconState>::from("consensus-spec-tests/tests/mainnet/bellatrix/rewards/random/pyspec_tests/full_random_low_balances_1");
 
-    test_case.execute();
+    test_case.execute(|state, context| {
+        let flag_index = spec::TIMELY_SOURCE_FLAG_INDEX;
+        let source_deltas = spec::get_flag_index_deltas(state, flag_index, context).unwrap();
+        let flag_index = spec::TIMELY_TARGET_FLAG_INDEX;
+        let target_deltas = spec::get_flag_index_deltas(state, flag_index, context).unwrap();
+        let flag_index = spec::TIMELY_HEAD_FLAG_INDEX;
+        let head_deltas = spec::get_flag_index_deltas(state, flag_index, context).unwrap();
+        let inactivity_penalty_deltas =
+            spec::get_inactivity_penalty_deltas(state, context).unwrap();
+        (
+            source_deltas,
+            target_deltas,
+            head_deltas,
+            None,
+            inactivity_penalty_deltas,
+        )
+    });
 }
 
 #[test]
 fn test_full_random_misc_balances() {
-    let  test_case = RandomTestCase::from("consensus-spec-tests/tests/mainnet/bellatrix/rewards/random/pyspec_tests/full_random_misc_balances");
+    let  test_case = RandomTestCase::<spec::BeaconState>::from("consensus-spec-tests/tests/mainnet/bellatrix/rewards/random/pyspec_tests/full_random_misc_balances");
 
-    test_case.execute();
+    test_case.execute(|state, context| {
+        let flag_index = spec::TIMELY_SOURCE_FLAG_INDEX;
+        let source_deltas = spec::get_flag_index_deltas(state, flag_index, context).unwrap();
+        let flag_index = spec::TIMELY_TARGET_FLAG_INDEX;
+        let target_deltas = spec::get_flag_index_deltas(state, flag_index, context).unwrap();
+        let flag_index = spec::TIMELY_HEAD_FLAG_INDEX;
+        let head_deltas = spec::get_flag_index_deltas(state, flag_index, context).unwrap();
+        let inactivity_penalty_deltas =
+            spec::get_inactivity_penalty_deltas(state, context).unwrap();
+        (
+            source_deltas,
+            target_deltas,
+            head_deltas,
+            None,
+            inactivity_penalty_deltas,
+        )
+    });
 }
 
 #[test]
 fn test_full_random_without_leak_0() {
-    let  test_case = RandomTestCase::from("consensus-spec-tests/tests/mainnet/bellatrix/rewards/random/pyspec_tests/full_random_without_leak_0");
+    let  test_case = RandomTestCase::<spec::BeaconState>::from("consensus-spec-tests/tests/mainnet/bellatrix/rewards/random/pyspec_tests/full_random_without_leak_0");
 
-    test_case.execute();
+    test_case.execute(|state, context| {
+        let flag_index = spec::TIMELY_SOURCE_FLAG_INDEX;
+        let source_deltas = spec::get_flag_index_deltas(state, flag_index, context).unwrap();
+        let flag_index = spec::TIMELY_TARGET_FLAG_INDEX;
+        let target_deltas = spec::get_flag_index_deltas(state, flag_index, context).unwrap();
+        let flag_index = spec::TIMELY_HEAD_FLAG_INDEX;
+        let head_deltas = spec::get_flag_index_deltas(state, flag_index, context).unwrap();
+        let inactivity_penalty_deltas =
+            spec::get_inactivity_penalty_deltas(state, context).unwrap();
+        (
+            source_deltas,
+            target_deltas,
+            head_deltas,
+            None,
+            inactivity_penalty_deltas,
+        )
+    });
 }
 
 #[test]
 fn test_full_random_without_leak_and_current_exit_0() {
-    let  test_case = RandomTestCase::from("consensus-spec-tests/tests/mainnet/bellatrix/rewards/random/pyspec_tests/full_random_without_leak_and_current_exit_0");
+    let  test_case = RandomTestCase::<spec::BeaconState>::from("consensus-spec-tests/tests/mainnet/bellatrix/rewards/random/pyspec_tests/full_random_without_leak_and_current_exit_0");
 
-    test_case.execute();
+    test_case.execute(|state, context| {
+        let flag_index = spec::TIMELY_SOURCE_FLAG_INDEX;
+        let source_deltas = spec::get_flag_index_deltas(state, flag_index, context).unwrap();
+        let flag_index = spec::TIMELY_TARGET_FLAG_INDEX;
+        let target_deltas = spec::get_flag_index_deltas(state, flag_index, context).unwrap();
+        let flag_index = spec::TIMELY_HEAD_FLAG_INDEX;
+        let head_deltas = spec::get_flag_index_deltas(state, flag_index, context).unwrap();
+        let inactivity_penalty_deltas =
+            spec::get_inactivity_penalty_deltas(state, context).unwrap();
+        (
+            source_deltas,
+            target_deltas,
+            head_deltas,
+            None,
+            inactivity_penalty_deltas,
+        )
+    });
 }
--- tests/consensus_spec_tests/minimal/altair/rewards/basic/pyspec_tests/mod.rs
+++ tests/consensus_spec_tests/minimal/altair/rewards/basic/pyspec_tests/mod.rs
@@ -2,88 +2,266 @@
 // This file was generated with `gen-tests`. Do NOT edit manually.
 
 use crate::spec_test_runners::rewards::BasicTestCase;
+use ethereum_consensus::altair::minimal as spec;
+use ssz_rs::prelude::*;
 
 #[test]
 fn test_all_balances_too_low_for_reward() {
-    let  test_case = BasicTestCase::from("consensus-spec-tests/tests/minimal/altair/rewards/basic/pyspec_tests/all_balances_too_low_for_reward");
+    let  test_case = BasicTestCase::<spec::BeaconState>::from("consensus-spec-tests/tests/minimal/altair/rewards/basic/pyspec_tests/all_balances_too_low_for_reward");
 
-    test_case.execute();
+    test_case.execute(|state, context| {
+        let flag_index = spec::TIMELY_SOURCE_FLAG_INDEX;
+        let source_deltas = spec::get_flag_index_deltas(state, flag_index, context).unwrap();
+        let flag_index = spec::TIMELY_TARGET_FLAG_INDEX;
+        let target_deltas = spec::get_flag_index_deltas(state, flag_index, context).unwrap();
+        let flag_index = spec::TIMELY_HEAD_FLAG_INDEX;
+        let head_deltas = spec::get_flag_index_deltas(state, flag_index, context).unwrap();
+        let inactivity_penalty_deltas =
+            spec::get_inactivity_penalty_deltas(state, context).unwrap();
+        (
+            source_deltas,
+            target_deltas,
+            head_deltas,
+            None,
+            inactivity_penalty_deltas,
+        )
+    });
 }
 
 #[test]
 fn test_empty() {
-    let test_case = BasicTestCase::from(
+    let test_case = BasicTestCase::<spec::BeaconState>::from(
         "consensus-spec-tests/tests/minimal/altair/rewards/basic/pyspec_tests/empty",
     );
 
-    test_case.execute();
+    test_case.execute(|state, context| {
+        let flag_index = spec::TIMELY_SOURCE_FLAG_INDEX;
+        let source_deltas = spec::get_flag_index_deltas(state, flag_index, context).unwrap();
+        let flag_index = spec::TIMELY_TARGET_FLAG_INDEX;
+        let target_deltas = spec::get_flag_index_deltas(state, flag_index, context).unwrap();
+        let flag_index = spec::TIMELY_HEAD_FLAG_INDEX;
+        let head_deltas = spec::get_flag_index_deltas(state, flag_index, context).unwrap();
+        let inactivity_penalty_deltas =
+            spec::get_inactivity_penalty_deltas(state, context).unwrap();
+        (
+            source_deltas,
+            target_deltas,
+            head_deltas,
+            None,
+            inactivity_penalty_deltas,
+        )
+    });
 }
 
 #[test]
 fn test_full_all_correct() {
-    let test_case = BasicTestCase::from(
+    let test_case = BasicTestCase::<spec::BeaconState>::from(
         "consensus-spec-tests/tests/minimal/altair/rewards/basic/pyspec_tests/full_all_correct",
     );
 
-    test_case.execute();
+    test_case.execute(|state, context| {
+        let flag_index = spec::TIMELY_SOURCE_FLAG_INDEX;
+        let source_deltas = spec::get_flag_index_deltas(state, flag_index, context).unwrap();
+        let flag_index = spec::TIMELY_TARGET_FLAG_INDEX;
+        let target_deltas = spec::get_flag_index_deltas(state, flag_index, context).unwrap();
+        let flag_index = spec::TIMELY_HEAD_FLAG_INDEX;
+        let head_deltas = spec::get_flag_index_deltas(state, flag_index, context).unwrap();
+        let inactivity_penalty_deltas =
+            spec::get_inactivity_penalty_deltas(state, context).unwrap();
+        (
+            source_deltas,
+            target_deltas,
+            head_deltas,
+            None,
+            inactivity_penalty_deltas,
+        )
+    });
 }
 
 #[test]
 fn test_full_but_partial_participation() {
-    let  test_case = BasicTestCase::from("consensus-spec-tests/tests/minimal/altair/rewards/basic/pyspec_tests/full_but_partial_participation");
+    let  test_case = BasicTestCase::<spec::BeaconState>::from("consensus-spec-tests/tests/minimal/altair/rewards/basic/pyspec_tests/full_but_partial_participation");
 
-    test_case.execute();
+    test_case.execute(|state, context| {
+        let flag_index = spec::TIMELY_SOURCE_FLAG_INDEX;
+        let source_deltas = spec::get_flag_index_deltas(state, flag_index, context).unwrap();
+        let flag_index = spec::TIMELY_TARGET_FLAG_INDEX;
+        let target_deltas = spec::get_flag_index_deltas(state, flag_index, context).unwrap();
+        let flag_index = spec::TIMELY_HEAD_FLAG_INDEX;
+        let head_deltas = spec::get_flag_index_deltas(state, flag_index, context).unwrap();
+        let inactivity_penalty_deltas =
+            spec::get_inactivity_penalty_deltas(state, context).unwrap();
+        (
+            source_deltas,
+            target_deltas,
+            head_deltas,
+            None,
+            inactivity_penalty_deltas,
+        )
+    });
 }
 
 #[test]
 fn test_half_full() {
-    let test_case = BasicTestCase::from(
+    let test_case = BasicTestCase::<spec::BeaconState>::from(
         "consensus-spec-tests/tests/minimal/altair/rewards/basic/pyspec_tests/half_full",
     );
 
-    test_case.execute();
+    test_case.execute(|state, context| {
+        let flag_index = spec::TIMELY_SOURCE_FLAG_INDEX;
+        let source_deltas = spec::get_flag_index_deltas(state, flag_index, context).unwrap();
+        let flag_index = spec::TIMELY_TARGET_FLAG_INDEX;
+        let target_deltas = spec::get_flag_index_deltas(state, flag_index, context).unwrap();
+        let flag_index = spec::TIMELY_HEAD_FLAG_INDEX;
+        let head_deltas = spec::get_flag_index_deltas(state, flag_index, context).unwrap();
+        let inactivity_penalty_deltas =
+            spec::get_inactivity_penalty_deltas(state, context).unwrap();
+        (
+            source_deltas,
+            target_deltas,
+            head_deltas,
+            None,
+            inactivity_penalty_deltas,
+        )
+    });
 }
 
 #[test]
 fn test_quarter_full() {
-    let test_case = BasicTestCase::from(
+    let test_case = BasicTestCase::<spec::BeaconState>::from(
         "consensus-spec-tests/tests/minimal/altair/rewards/basic/pyspec_tests/quarter_full",
     );
 
-    test_case.execute();
+    test_case.execute(|state, context| {
+        let flag_index = spec::TIMELY_SOURCE_FLAG_INDEX;
+        let source_deltas = spec::get_flag_index_deltas(state, flag_index, context).unwrap();
+        let flag_index = spec::TIMELY_TARGET_FLAG_INDEX;
+        let target_deltas = spec::get_flag_index_deltas(state, flag_index, context).unwrap();
+        let flag_index = spec::TIMELY_HEAD_FLAG_INDEX;
+        let head_deltas = spec::get_flag_index_deltas(state, flag_index, context).unwrap();
+        let inactivity_penalty_deltas =
+            spec::get_inactivity_penalty_deltas(state, context).unwrap();
+        (
+            source_deltas,
+            target_deltas,
+            head_deltas,
+            None,
+            inactivity_penalty_deltas,
+        )
+    });
 }
 
 #[test]
 fn test_some_very_low_effective_balances_that_attested() {
-    let  test_case = BasicTestCase::from("consensus-spec-tests/tests/minimal/altair/rewards/basic/pyspec_tests/some_very_low_effective_balances_that_attested");
+    let  test_case = BasicTestCase::<spec::BeaconState>::from("consensus-spec-tests/tests/minimal/altair/rewards/basic/pyspec_tests/some_very_low_effective_balances_that_attested");
 
-    test_case.execute();
+    test_case.execute(|state, context| {
+        let flag_index = spec::TIMELY_SOURCE_FLAG_INDEX;
+        let source_deltas = spec::get_flag_index_deltas(state, flag_index, context).unwrap();
+        let flag_index = spec::TIMELY_TARGET_FLAG_INDEX;
+        let target_deltas = spec::get_flag_index_deltas(state, flag_index, context).unwrap();
+        let flag_index = spec::TIMELY_HEAD_FLAG_INDEX;
+        let head_deltas = spec::get_flag_index_deltas(state, flag_index, context).unwrap();
+        let inactivity_penalty_deltas =
+            spec::get_inactivity_penalty_deltas(state, context).unwrap();
+        (
+            source_deltas,
+            target_deltas,
+            head_deltas,
+            None,
+            inactivity_penalty_deltas,
+        )
+    });
 }
 
 #[test]
 fn test_some_very_low_effective_balances_that_did_not_attest() {
-    let  test_case = BasicTestCase::from("consensus-spec-tests/tests/minimal/altair/rewards/basic/pyspec_tests/some_very_low_effective_balances_that_did_not_attest");
+    let  test_case = BasicTestCase::<spec::BeaconState>::from("consensus-spec-tests/tests/minimal/altair/rewards/basic/pyspec_tests/some_very_low_effective_balances_that_did_not_attest");
 
-    test_case.execute();
+    test_case.execute(|state, context| {
+        let flag_index = spec::TIMELY_SOURCE_FLAG_INDEX;
+        let source_deltas = spec::get_flag_index_deltas(state, flag_index, context).unwrap();
+        let flag_index = spec::TIMELY_TARGET_FLAG_INDEX;
+        let target_deltas = spec::get_flag_index_deltas(state, flag_index, context).unwrap();
+        let flag_index = spec::TIMELY_HEAD_FLAG_INDEX;
+        let head_deltas = spec::get_flag_index_deltas(state, flag_index, context).unwrap();
+        let inactivity_penalty_deltas =
+            spec::get_inactivity_penalty_deltas(state, context).unwrap();
+        (
+            source_deltas,
+            target_deltas,
+            head_deltas,
+            None,
+            inactivity_penalty_deltas,
+        )
+    });
 }
 
 #[test]
 fn test_with_exited_validators() {
-    let  test_case = BasicTestCase::from("consensus-spec-tests/tests/minimal/altair/rewards/basic/pyspec_tests/with_exited_validators");
+    let  test_case = BasicTestCase::<spec::BeaconState>::from("consensus-spec-tests/tests/minimal/altair/rewards/basic/pyspec_tests/with_exited_validators");
 
-    test_case.execute();
+    test_case.execute(|state, context| {
+        let flag_index = spec::TIMELY_SOURCE_FLAG_INDEX;
+        let source_deltas = spec::get_flag_index_deltas(state, flag_index, context).unwrap();
+        let flag_index = spec::TIMELY_TARGET_FLAG_INDEX;
+        let target_deltas = spec::get_flag_index_deltas(state, flag_index, context).unwrap();
+        let flag_index = spec::TIMELY_HEAD_FLAG_INDEX;
+        let head_deltas = spec::get_flag_index_deltas(state, flag_index, context).unwrap();
+        let inactivity_penalty_deltas =
+            spec::get_inactivity_penalty_deltas(state, context).unwrap();
+        (
+            source_deltas,
+            target_deltas,
+            head_deltas,
+            None,
+            inactivity_penalty_deltas,
+        )
+    });
 }
 
 #[test]
 fn test_with_not_yet_activated_validators() {
-    let  test_case = BasicTestCase::from("consensus-spec-tests/tests/minimal/altair/rewards/basic/pyspec_tests/with_not_yet_activated_validators");
+    let  test_case = BasicTestCase::<spec::BeaconState>::from("consensus-spec-tests/tests/minimal/altair/rewards/basic/pyspec_tests/with_not_yet_activated_validators");
 
-    test_case.execute();
+    test_case.execute(|state, context| {
+        let flag_index = spec::TIMELY_SOURCE_FLAG_INDEX;
+        let source_deltas = spec::get_flag_index_deltas(state, flag_index, context).unwrap();
+        let flag_index = spec::TIMELY_TARGET_FLAG_INDEX;
+        let target_deltas = spec::get_flag_index_deltas(state, flag_index, context).unwrap();
+        let flag_index = spec::TIMELY_HEAD_FLAG_INDEX;
+        let head_deltas = spec::get_flag_index_deltas(state, flag_index, context).unwrap();
+        let inactivity_penalty_deltas =
+            spec::get_inactivity_penalty_deltas(state, context).unwrap();
+        (
+            source_deltas,
+            target_deltas,
+            head_deltas,
+            None,
+            inactivity_penalty_deltas,
+        )
+    });
 }
 
 #[test]
 fn test_with_slashed_validators() {
-    let  test_case = BasicTestCase::from("consensus-spec-tests/tests/minimal/altair/rewards/basic/pyspec_tests/with_slashed_validators");
+    let  test_case = BasicTestCase::<spec::BeaconState>::from("consensus-spec-tests/tests/minimal/altair/rewards/basic/pyspec_tests/with_slashed_validators");
 
-    test_case.execute();
+    test_case.execute(|state, context| {
+        let flag_index = spec::TIMELY_SOURCE_FLAG_INDEX;
+        let source_deltas = spec::get_flag_index_deltas(state, flag_index, context).unwrap();
+        let flag_index = spec::TIMELY_TARGET_FLAG_INDEX;
+        let target_deltas = spec::get_flag_index_deltas(state, flag_index, context).unwrap();
+        let flag_index = spec::TIMELY_HEAD_FLAG_INDEX;
+        let head_deltas = spec::get_flag_index_deltas(state, flag_index, context).unwrap();
+        let inactivity_penalty_deltas =
+            spec::get_inactivity_penalty_deltas(state, context).unwrap();
+        (
+            source_deltas,
+            target_deltas,
+            head_deltas,
+            None,
+            inactivity_penalty_deltas,
+        )
+    });
 }
