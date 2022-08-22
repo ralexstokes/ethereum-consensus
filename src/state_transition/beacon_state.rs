2f37984a4a2fc2a2e6d612213aa5fe7b58918854
Alex Stokes <r.alex.stokes@gmail.com>
2022-08-22 13:57:45 -0600

    add convenience accessors for the state

--- src/state_transition/beacon_state.rs
+++ src/state_transition/beacon_state.rs
@@ -252,3 +252,99 @@ impl<
         Self::Bellatrix(Box::new(state))
     }
 }
+
+impl<
+        const SLOTS_PER_HISTORICAL_ROOT: usize,
+        const HISTORICAL_ROOTS_LIMIT: usize,
+        const ETH1_DATA_VOTES_BOUND: usize,
+        const VALIDATOR_REGISTRY_LIMIT: usize,
+        const EPOCHS_PER_HISTORICAL_VECTOR: usize,
+        const EPOCHS_PER_SLASHINGS_VECTOR: usize,
+        const MAX_VALIDATORS_PER_COMMITTEE: usize,
+        const PENDING_ATTESTATIONS_BOUND: usize,
+        const SYNC_COMMITTEE_SIZE: usize,
+        const BYTES_PER_LOGS_BLOOM: usize,
+        const MAX_EXTRA_DATA_BYTES: usize,
+        const MAX_BYTES_PER_TRANSACTION: usize,
+        const MAX_TRANSACTIONS_PER_PAYLOAD: usize,
+    >
+    BeaconState<
+        SLOTS_PER_HISTORICAL_ROOT,
+        HISTORICAL_ROOTS_LIMIT,
+        ETH1_DATA_VOTES_BOUND,
+        VALIDATOR_REGISTRY_LIMIT,
+        EPOCHS_PER_HISTORICAL_VECTOR,
+        EPOCHS_PER_SLASHINGS_VECTOR,
+        MAX_VALIDATORS_PER_COMMITTEE,
+        PENDING_ATTESTATIONS_BOUND,
+        SYNC_COMMITTEE_SIZE,
+        BYTES_PER_LOGS_BLOOM,
+        MAX_EXTRA_DATA_BYTES,
+        MAX_BYTES_PER_TRANSACTION,
+        MAX_TRANSACTIONS_PER_PAYLOAD,
+    >
+{
+    pub fn phase0(
+        self,
+    ) -> Option<
+        phase0::BeaconState<
+            SLOTS_PER_HISTORICAL_ROOT,
+            HISTORICAL_ROOTS_LIMIT,
+            ETH1_DATA_VOTES_BOUND,
+            VALIDATOR_REGISTRY_LIMIT,
+            EPOCHS_PER_HISTORICAL_VECTOR,
+            EPOCHS_PER_SLASHINGS_VECTOR,
+            MAX_VALIDATORS_PER_COMMITTEE,
+            PENDING_ATTESTATIONS_BOUND,
+        >,
+    > {
+        match self {
+            Self::Phase0(state) => Some(*state),
+            _ => None,
+        }
+    }
+
+    pub fn altair(
+        self,
+    ) -> Option<
+        altair::BeaconState<
+            SLOTS_PER_HISTORICAL_ROOT,
+            HISTORICAL_ROOTS_LIMIT,
+            ETH1_DATA_VOTES_BOUND,
+            VALIDATOR_REGISTRY_LIMIT,
+            EPOCHS_PER_HISTORICAL_VECTOR,
+            EPOCHS_PER_SLASHINGS_VECTOR,
+            MAX_VALIDATORS_PER_COMMITTEE,
+            SYNC_COMMITTEE_SIZE,
+        >,
+    > {
+        match self {
+            Self::Altair(state) => Some(*state),
+            _ => None,
+        }
+    }
+
+    pub fn bellatrix(
+        self,
+    ) -> Option<
+        bellatrix::BeaconState<
+            SLOTS_PER_HISTORICAL_ROOT,
+            HISTORICAL_ROOTS_LIMIT,
+            ETH1_DATA_VOTES_BOUND,
+            VALIDATOR_REGISTRY_LIMIT,
+            EPOCHS_PER_HISTORICAL_VECTOR,
+            EPOCHS_PER_SLASHINGS_VECTOR,
+            MAX_VALIDATORS_PER_COMMITTEE,
+            SYNC_COMMITTEE_SIZE,
+            BYTES_PER_LOGS_BLOOM,
+            MAX_EXTRA_DATA_BYTES,
+            MAX_BYTES_PER_TRANSACTION,
+            MAX_TRANSACTIONS_PER_PAYLOAD,
+        >,
+    > {
+        match self {
+            Self::Bellatrix(state) => Some(*state),
+            _ => None,
+        }
+    }
+}
