d0f48b2228e2f083c2bf1d3537125b599855d058
Joseph Cook <33655003+jmcook1186@users.noreply.github.com>
2022-10-25 16:26:32 +0100

    fix fmt

--- src/api_client.rs
+++ src/api_client.rs
@@ -492,8 +492,7 @@ impl Client {
         let dependent_root_value = result
             .meta
             .remove("dependent_root")
-            .ok_or_else(|| {
-            Error::MissingExpectedData("`dependent_root`".to_string())})?;
+            .ok_or_else(|| Error::MissingExpectedData("`dependent_root`".to_string()))?;
         let dependent_root: Root = serde_json::from_value(dependent_root_value)?;
         Ok((dependent_root, result.data))
     }
