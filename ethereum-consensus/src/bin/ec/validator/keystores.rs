6c08605e43abf9b2bc342d4024b342c4f5f38529
Alex Stokes <r.alex.stokes@gmail.com>
2023-09-27 09:46:08 -0600

    finish EIP-2333 impl

--- ethereum-consensus/src/bin/ec/validator/keystores.rs
+++ ethereum-consensus/src/bin/ec/validator/keystores.rs
@@ -6,12 +6,15 @@ use ruint::{aliases::U256, uint, Uint};
 use sha2::Sha256;
 use thiserror::Error;
 
-type U381 = Uint<381, 6>;
+type U384 = Uint<384, 6>;
 
 const SALT: &[u8; 20] = b"BLS-SIG-KEYGEN-SALT-";
-const L: u8 = 48;
-const R: U381 =
-    uint!(52435875175126190479447740508185965837690552500527637822603658699938581184513_U381);
+const L: usize = 48;
+const R: U384 =
+    uint!(52435875175126190479447740508185965837690552500527637822603658699938581184513_U384);
+const K: usize = 32;
+const LAMPORT_COUNT: usize = 255;
+const LAMPORT_L: usize = K * LAMPORT_COUNT;
 
 #[derive(Debug, Error)]
 pub enum Error {}
@@ -24,45 +27,68 @@ pub struct Keystore {
     key: Key,
 }
 
-fn ikm_to_lamport_secret_key(ikm: &[u8], salt: &[u8]) -> [[u8; 32]; 255] {
-    Default::default()
+// NOTE: we don't need `mut` on `input` but it keeps the lifetimes simple and allows us to skip
+// unnecessary allocations
+fn bytes_split<const M: usize, const N: usize>(input: &mut [u8]) -> Vec<&[u8]> {
+    debug_assert!(M % N == 0);
+    debug_assert!(input.len() == M);
+
+    input.chunks_exact(N).collect()
+}
+
+fn ikm_to_lamport_secret_key<'a>(ikm: &[u8], salt: &[u8], output: &'a mut [u8]) -> Vec<&'a [u8]> {
+    let hk = Hkdf::<Sha256>::new(Some(salt.as_ref()), &ikm);
+    hk.expand(&[], output).expect("length L is valid");
+    bytes_split::<LAMPORT_L, K>(output)
+}
+
+fn flip_bits(input: [u8; 32]) -> Vec<u8> {
+    input.into_iter().map(|i| !i).collect()
 }
 
 fn parent_key_to_lamport_public_key(key: &Key, index: u32) -> [u8; 32] {
     let salt = index.to_be_bytes();
     let ikm: [u8; 32] = key.0.to_be_bytes();
-    let lamport_0 = ikm_to_lamport_secret_key(&ikm, &salt);
-    let not_ikm = flip_bits(&ikm);
-    let lamport_1 = ikm_to_lamport_secret_key(&not_ikm, &salt);
-    let lamport_public_key = Vec::with_capacity(8160);
-    for i in 1..=255 {
-        //
-    }
-    for i in 1..=255 {
-        //
-    }
+    let mut output_0 = [0u8; LAMPORT_L];
+    let lamport_0 = ikm_to_lamport_secret_key(&ikm, &salt, &mut output_0);
+    let not_ikm = flip_bits(ikm);
+    let mut output_1 = [0u8; LAMPORT_L];
+    let lamport_1 = ikm_to_lamport_secret_key(&not_ikm, &salt, &mut output_1);
+    let mut lamport_public_key = Vec::with_capacity(LAMPORT_L);
+    lamport_0.into_iter().map(hash).for_each(|data| {
+        lamport_public_key.extend_from_slice(data.as_ref());
+    });
+    lamport_1.into_iter().map(hash).for_each(|data| {
+        lamport_public_key.extend_from_slice(data.as_ref());
+    });
+    debug_assert_eq!(lamport_public_key.len(), 16320);
     let compressed_lamport_public_key = hash(&lamport_public_key);
     compressed_lamport_public_key.as_ref().try_into().unwrap()
 }
 
 fn hkdf_mod_r(input: &[u8]) -> Key {
-    let mut key = U381::ZERO;
+    let mut key = U384::ZERO;
     let mut salt = hash(SALT);
-    let key_info = [0, L];
+    // safety: `L` fits in type by definition
+    let l = L as u8;
+    let key_info = [0, l];
     let mut ikm = input.to_vec();
     ikm.push(0);
 
-    while key == U381::ZERO {
+    while key == U384::ZERO {
         let hk = Hkdf::<Sha256>::new(Some(salt.as_ref()), &ikm);
-        let mut okm = [0u8; 48];
+        let mut okm = [0u8; L];
         hk.expand(&key_info, &mut okm).expect("length L is valid");
-        let inner = U381::from_be_bytes(okm);
+        let inner = U384::from_be_bytes(okm);
         key = inner % R;
 
         salt = hash(&salt);
     }
 
-    let key_bytes: [u8; 48] = key.to_be_bytes();
+    // ensure we are in the field
+    debug_assert_eq!(key % R, key);
+
+    let key_bytes: [u8; L] = key.to_be_bytes();
     let inner: [u8; 32] = key_bytes[16..].try_into().unwrap();
     Key(U256::from_be_bytes(inner))
 }
