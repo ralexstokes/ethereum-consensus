99f62e50b4bc184fd265f469ac4c3828134bc751
EchoAlice <echoalice0@gmail.com>
2023-10-06 11:27:28 -0600

    Use custom KzgCommitment and KzgProof types as arguments to relevant functions

--- ethereum-consensus/src/deneb/polynomial_commitments.rs
+++ ethereum-consensus/src/deneb/polynomial_commitments.rs
@@ -1,6 +1,6 @@
 use crate::{deneb::blob_sidecar::Blob, primitives::Bytes32, ssz::prelude::*};
+use c_kzg::Error;
 pub use c_kzg::KzgSettings;
-use c_kzg::{Bytes48, Error};
 
 use super::mainnet::FIELD_ELEMENTS_PER_BLOB;
 
@@ -64,13 +64,13 @@ pub fn compute_kzg_proof<const BYTES_PER_BLOB: usize>(
 // TODO: Map error
 pub fn compute_blob_kzg_proof<const BYTES_PER_BLOB: usize>(
     blob: &Blob<BYTES_PER_BLOB>,
-    commitment_bytes: &Bytes48,
+    commitment: &KzgCommitment,
     kzg_settings: &KzgSettings,
 ) -> Result<KzgProof, Error> {
     let blob = c_kzg::Blob::from_bytes(blob.as_ref()).unwrap();
+    let commitment = c_kzg::Bytes48::from_bytes(commitment.0.as_ref()).unwrap();
 
-    let ckzg_proof =
-        c_kzg::KzgProof::compute_blob_kzg_proof(&blob, &commitment_bytes, kzg_settings)?;
+    let ckzg_proof = c_kzg::KzgProof::compute_blob_kzg_proof(&blob, &commitment, kzg_settings)?;
 
     let bytes_proof = ckzg_proof.to_bytes();
     let proof = ByteVector::try_from(bytes_proof.as_ref()).unwrap();
@@ -79,20 +79,22 @@ pub fn compute_blob_kzg_proof<const BYTES_PER_BLOB: usize>(
 
 // TODO: Map error
 pub fn verify_kzg_proof(
-    commitment_bytes: &Bytes48,
+    commitment: &KzgCommitment,
     evaluation_point: &FieldElement,
     result_point: &FieldElement,
-    proof_bytes: &Bytes48,
+    proof: &KzgProof,
     kzg_settings: &KzgSettings,
 ) -> Result<bool, Error> {
     let evaluation_point = c_kzg::Bytes32::from_bytes(evaluation_point.as_ref())?;
     let result_point = c_kzg::Bytes32::from_bytes(result_point.as_ref())?;
+    let commitment = c_kzg::Bytes48::from_bytes(commitment.0.as_ref()).unwrap();
+    let proof = c_kzg::Bytes48::from_bytes(proof.0.as_ref()).unwrap();
 
     let out = c_kzg::KzgProof::verify_kzg_proof(
-        commitment_bytes,
+        &commitment,
         &evaluation_point,
         &result_point,
-        proof_bytes,
+        &proof,
         kzg_settings,
     )?;
     Ok(out)
@@ -101,38 +103,47 @@ pub fn verify_kzg_proof(
 // TODO: Map error
 pub fn verify_blob_kzg_proof<const BYTES_PER_BLOB: usize>(
     blob: &Blob<BYTES_PER_BLOB>,
-    commitment_bytes: &Bytes48,
-    proof_bytes: &Bytes48,
+    commitment: &KzgCommitment,
+    proof: &KzgProof,
     kzg_settings: &KzgSettings,
 ) -> Result<bool, Error> {
     let blob = c_kzg::Blob::from_bytes(blob.as_ref()).unwrap();
+    let commitment = c_kzg::Bytes48::from_bytes(commitment.0.as_ref()).unwrap();
+    let proof = c_kzg::Bytes48::from_bytes(proof.0.as_ref()).unwrap();
 
-    let out = c_kzg::KzgProof::verify_blob_kzg_proof(
-        &blob,
-        &commitment_bytes,
-        &proof_bytes,
-        kzg_settings,
-    )?;
+    let out = c_kzg::KzgProof::verify_blob_kzg_proof(&blob, &commitment, &proof, kzg_settings)?;
     Ok(out)
 }
 
 pub fn verify_blob_kzg_proof_batch<const BYTES_PER_BLOB: usize>(
     blobs: &[Blob<BYTES_PER_BLOB>],
-    commitments_bytes: &[Bytes48],
-    proofs_bytes: &[Bytes48],
+    commitments: &[KzgCommitment],
+    proofs: &[KzgProof],
     kzg_settings: &KzgSettings,
 ) -> Result<bool, Error> {
     let mut c_kzg_blobs = Vec::with_capacity(blobs.len());
+    let mut c_kzg_commitments = Vec::with_capacity(commitments.len());
+    let mut c_kzg_proofs = Vec::with_capacity(proofs.len());
 
+    // TODO: Can we assume there are *always* the same number of
+    //       blobs, commitments and proofs?  Wanting to use one for loop.
     for blob in blobs {
         let blob = c_kzg::Blob::from_bytes(blob.as_ref())?;
         c_kzg_blobs.push(blob);
     }
+    for commitment in commitments {
+        let commitment = c_kzg::Bytes48::from_bytes(commitment.0.as_ref()).unwrap();
+        c_kzg_commitments.push(commitment);
+    }
+    for proof in proofs {
+        let proof = c_kzg::Bytes48::from_bytes(proof.0.as_ref()).unwrap();
+        c_kzg_proofs.push(proof);
+    }
 
     c_kzg::KzgProof::verify_blob_kzg_proof_batch(
         &c_kzg_blobs,
-        commitments_bytes,
-        proofs_bytes,
+        &c_kzg_commitments,
+        &c_kzg_proofs,
         kzg_settings,
     )
     .map_err(Into::into)
