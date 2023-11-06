d94baebc243f2f9f17b477024e0ace6e0a7c2737
EchoAlice <echoalice0@gmail.com>
2023-11-06 15:22:38 -0700

    Create verify_blob_sidecar_inclusion_proof()

--- ethereum-consensus/src/deneb/blob_sidecar.rs
+++ ethereum-consensus/src/deneb/blob_sidecar.rs
@@ -27,6 +27,28 @@ pub struct BlobSidecar<
     pub kzg_commitment_inclusion_proof: Vector<Bytes32, KZG_COMMITMENT_INCLUSION_PROOF_DEPTH>,
 }
 
+impl<const BYTES_PER_BLOB: usize, const KZG_COMMITMENT_INCLUSION_PROOF_DEPTH: usize>
+    BlobSidecar<BYTES_PER_BLOB, KZG_COMMITMENT_INCLUSION_PROOF_DEPTH>
+{
+    pub fn verify_blob_sidecar_inclusion_proof(&mut self) -> bool {
+        // TODO: Calculate real gindex
+        let gindex = 4;
+
+        // TODO: Handle hash_tree_root() error
+        let leaf = &self.kzg_commitment.hash_tree_root().unwrap();
+        let branch: Vec<_> = self
+            .kzg_commitment_inclusion_proof
+            .iter()
+            .map(|bytes| Node::try_from(bytes.as_slice()).unwrap())
+            .collect();
+        let depth = KZG_COMMITMENT_INCLUSION_PROOF_DEPTH;
+        let index = gindex;
+        let root = &self.signed_block_header.message.body_root;
+
+        is_valid_merkle_branch(leaf, branch.iter(), depth, index, root)
+    }
+}
+
 #[derive(
     Default, Debug, Clone, SimpleSerialize, PartialEq, Eq, serde::Serialize, serde::Deserialize,
 )]
