72eafc863f2c5ebd319d9fddeb5d5347b009d588
Alex Stokes <r.alex.stokes@gmail.com>
2024-03-25 14:45:50 -0600

    impl `transition` tests

--- spec-tests/runners/transition.rs
+++ spec-tests/runners/transition.rs
@@ -4,7 +4,11 @@ use crate::{
     test_meta::{Config, Fork},
     test_utils::{load_snappy_ssz, load_yaml, Error},
 };
-use ethereum_consensus::{primitives::Epoch, state_transition::Context};
+use ethereum_consensus::{
+    primitives::Epoch,
+    state_transition::{self, Context},
+    types::{BeaconState, SignedBeaconBlock},
+};
 use serde::Deserialize;
 
 #[derive(Deserialize)]
@@ -54,78 +58,22 @@ fn load_test<
     (pre, post, pre_blocks, post_blocks, meta)
 }
 
-fn run_test<S: Eq, T, B, C>(
-    _pre: S,
-    _expected: T,
-    mut _pre_blocks: Vec<B>,
-    mut _post_blocks: Vec<C>,
-    meta: &Meta,
-    _context: &Context,
-) -> Result<(), Error> {
-    todo!(
-        "read for now to silence warning... {} {}",
-        meta.post_fork,
-        meta.fork_epoch,
-        /*
-        let mut context = $context.clone();
-        let meta = $meta;
-        match meta.post_fork.as_ref() {
-            "altair" => {
-                context.altair_fork_epoch = meta.fork_epoch;
-                context.bellatrix_fork_epoch = Epoch::MAX;
-            }
-            "bellatrix" => {
-                context.altair_fork_epoch = 0;
-                context.bellatrix_fork_epoch = meta.fork_epoch;
-                todo!("set other epochs?");
-            }
-            _ => todo!(),
+fn set_fork_epochs(meta: &mut Meta, context: &mut Context) {
+    context.altair_fork_epoch = Epoch::MAX;
+    context.bellatrix_fork_epoch = Epoch::MAX;
+    context.capella_fork_epoch = Epoch::MAX;
+    context.deneb_fork_epoch = Epoch::MAX;
+
+    match meta.post_fork.as_ref() {
+        "altair" => {
+            context.altair_fork_epoch = meta.fork_epoch;
         }
-        let execution_engine = ethereum_consensus::bellatrix::DefaultExecutionEngine::default();
-        let mut executor = Executor::<
-            { spec::SLOTS_PER_HISTORICAL_ROOT },
-            { spec::HISTORICAL_ROOTS_LIMIT },
-            { spec::ETH1_DATA_VOTES_BOUND },
-            { spec::VALIDATOR_REGISTRY_LIMIT },
-            { spec::EPOCHS_PER_HISTORICAL_VECTOR },
-            { spec::EPOCHS_PER_SLASHINGS_VECTOR },
-            { spec::MAX_VALIDATORS_PER_COMMITTEE },
-            { phase0_spec::PENDING_ATTESTATIONS_BOUND },
-            { spec::SYNC_COMMITTEE_SIZE },
-            { bellatrix_spec::BYTES_PER_LOGS_BLOOM },
-            { bellatrix_spec::MAX_EXTRA_DATA_BYTES },
-            { bellatrix_spec::MAX_BYTES_PER_TRANSACTION },
-            { bellatrix_spec::MAX_TRANSACTIONS_PER_PAYLOAD },
-            { spec::MAX_PROPOSER_SLASHINGS },
-            { spec::MAX_ATTESTER_SLASHINGS },
-            { spec::MAX_ATTESTATIONS },
-            { spec::MAX_DEPOSITS },
-            { spec::MAX_VOLUNTARY_EXITS },
-            ethereum_consensus::bellatrix::DefaultExecutionEngine,
-        >::new($pre.into(), execution_engine.into(), context);
-        for block in $pre_blocks.into_iter() {
-            let mut block = block.into();
-            executor.apply_block(&mut block)?;
+        "bellatrix" => {
+            context.altair_fork_epoch = 0;
+            context.bellatrix_fork_epoch = meta.fork_epoch;
         }
-        for block in $post_blocks.into_iter() {
-            let mut block = block.into();
-            executor.apply_block(&mut block)?;
-        }
-        todo!(
-            /*
-                match executor.state {
-                    BeaconState::Altair(inner) => Ok(*inner),
-                    _ => unreachable!(),
-                }
-                if pre == $post {
-                    Ok(())
-                } else {
-                    Err(Error::InvalidState)
-                }
-             */
-        )
-        */
-    )
+        _ => todo!(),
+    }
 }
 
 pub fn dispatch(test: &TestCase) -> Result<(), Error> {
@@ -139,8 +87,25 @@ pub fn dispatch(test: &TestCase) -> Result<(), Error> {
                     gen_exec! {
                         test,
                         load_test,
-                        | (pre, expected, pre_blocks, post_blocks, meta): (pre_spec::BeaconState, spec::BeaconState, Vec<pre_spec::SignedBeaconBlock>, Vec<spec::SignedBeaconBlock>, Meta), context: &Context| {
-                            run_test(pre, expected, pre_blocks, post_blocks, &meta, context)
+                        | (pre, expected, pre_blocks, post_blocks, mut meta): (pre_spec::BeaconState, spec::BeaconState, Vec<pre_spec::SignedBeaconBlock>, Vec<spec::SignedBeaconBlock>, Meta), context: &Context| {
+                            assert_eq!(meta.post_fork, "altair");
+                            let mut context = context.clone();
+                            set_fork_epochs(&mut meta, &mut context);
+                            let mut executor = state_transition::mainnet::Executor::new(BeaconState::Phase0(pre), context);
+                            for block in pre_blocks.into_iter() {
+                                let mut block = SignedBeaconBlock::Phase0(block);
+                                executor.apply_block(&mut block)?;
+                            }
+                            for block in post_blocks.into_iter() {
+                                let mut block = SignedBeaconBlock::Altair(block);
+                                executor.apply_block(&mut block)?;
+                            }
+                            let post = executor.state.altair().unwrap();
+                            if post != &expected {
+                                Err(Error::InvalidState)
+                            } else {
+                                Ok(())
+                            }
                         }
                     }
                 }
@@ -151,14 +116,91 @@ pub fn dispatch(test: &TestCase) -> Result<(), Error> {
                     gen_exec! {
                         test,
                         load_test,
-                        | (pre, expected, pre_blocks, post_blocks, meta): (pre_spec::BeaconState, spec::BeaconState, Vec<pre_spec::SignedBeaconBlock>, Vec<spec::SignedBeaconBlock>, Meta), context: &Context| {
-                            run_test(pre, expected, pre_blocks, post_blocks, &meta, context)
+                        | (pre, expected, pre_blocks, post_blocks, mut meta): (pre_spec::BeaconState, spec::BeaconState, Vec<pre_spec::SignedBeaconBlock>, Vec<spec::SignedBeaconBlock>, Meta), context: &Context| {
+                            assert_eq!(meta.post_fork, "bellatrix");
+                            let mut context = context.clone();
+                            set_fork_epochs(&mut meta, &mut context);
+                            let mut executor = state_transition::mainnet::Executor::new(BeaconState::Altair(pre), context);
+                            for block in pre_blocks.into_iter() {
+                                let mut block = SignedBeaconBlock::Altair(block);
+                                executor.apply_block(&mut block)?;
+                            }
+                            for block in post_blocks.into_iter() {
+                                let mut block = SignedBeaconBlock::Bellatrix(block);
+                                executor.apply_block(&mut block)?;
+                            }
+                            let post = executor.state.bellatrix().unwrap();
+                            if post != &expected {
+                                Err(Error::InvalidState)
+                            } else {
+                                Ok(())
+                            }
                         }
                     }
                 }
-                _ => todo!("implement after `run_test`"),
+                _ => todo!(),
             },
-            Config::Minimal => todo!("implement after `run_test`"),
+            Config::Minimal => match test.meta.fork {
+                Fork::Altair => {
+                    use ethereum_consensus::{
+                        altair::minimal as spec, phase0::minimal as pre_spec,
+                    };
+                    gen_exec! {
+                        test,
+                        load_test,
+                        | (pre, expected, pre_blocks, post_blocks, mut meta): (pre_spec::BeaconState, spec::BeaconState, Vec<pre_spec::SignedBeaconBlock>, Vec<spec::SignedBeaconBlock>, Meta), context: &Context| {
+                            assert_eq!(meta.post_fork, "altair");
+                            let mut context = context.clone();
+                            set_fork_epochs(&mut meta, &mut context);
+                            let mut executor = state_transition::minimal::Executor::new(BeaconState::Phase0(pre), context);
+                            for block in pre_blocks.into_iter() {
+                                let mut block = SignedBeaconBlock::Phase0(block);
+                                executor.apply_block(&mut block)?;
+                            }
+                            for block in post_blocks.into_iter() {
+                                let mut block = SignedBeaconBlock::Altair(block);
+                                executor.apply_block(&mut block)?;
+                            }
+                            let post = executor.state.altair().unwrap();
+                            if post != &expected {
+                                Err(Error::InvalidState)
+                            } else {
+                                Ok(())
+                            }
+                        }
+                    }
+                }
+                Fork::Bellatrix => {
+                    use ethereum_consensus::{
+                        altair::minimal as pre_spec, bellatrix::minimal as spec,
+                    };
+                    gen_exec! {
+                        test,
+                        load_test,
+                        | (pre, expected, pre_blocks, post_blocks, mut meta): (pre_spec::BeaconState, spec::BeaconState, Vec<pre_spec::SignedBeaconBlock>, Vec<spec::SignedBeaconBlock>, Meta), context: &Context| {
+                            assert_eq!(meta.post_fork, "bellatrix");
+                            let mut context = context.clone();
+                            set_fork_epochs(&mut meta, &mut context);
+                            let mut executor = state_transition::minimal::Executor::new(BeaconState::Altair(pre), context);
+                            for block in pre_blocks.into_iter() {
+                                let mut block = SignedBeaconBlock::Altair(block);
+                                executor.apply_block(&mut block)?;
+                            }
+                            for block in post_blocks.into_iter() {
+                                let mut block = SignedBeaconBlock::Bellatrix(block);
+                                executor.apply_block(&mut block)?;
+                            }
+                            let post = executor.state.bellatrix().unwrap();
+                            if post != &expected {
+                                Err(Error::InvalidState)
+                            } else {
+                                Ok(())
+                            }
+                        }
+                    }
+                }
+                _ => todo!(),
+            },
             config => unreachable!("no tests for {config:?}"),
         },
         handler => unreachable!("no tests for {handler}"),
