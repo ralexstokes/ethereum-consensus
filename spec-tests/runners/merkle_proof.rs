6ff5f500e819fd2d5268ca00b08e66c6a4a68d32
Alex Stokes <r.alex.stokes@gmail.com>
2024-03-31 12:34:55 -0600

    extend merkle proof tests with verification of proof generation

--- spec-tests/runners/merkle_proof.rs
+++ spec-tests/runners/merkle_proof.rs
@@ -3,21 +3,18 @@ use crate::{
     test_case::TestCase,
     test_utils::{load_snappy_ssz, load_yaml, Error},
 };
-use ethereum_consensus::{
-    primitives::{Bytes32, Root},
-    Error as SpecError,
-};
+use ethereum_consensus::Error as SpecError;
 use serde::Deserialize;
 use ssz_rs::{
     prelude::*,
-    proofs::{get_subtree_index, log_2},
+    proofs::{get_subtree_index, is_valid_merkle_branch_for_generalized_index, log_2, prove},
 };
 
 #[derive(Debug, Deserialize)]
 pub struct Proof {
-    leaf: Root,
+    leaf: Node,
     leaf_index: GeneralizedIndex,
-    branch: Vec<Bytes32>,
+    branch: Vec<Node>,
 }
 
 fn load_test<O: ssz_rs::Deserialize>(test_case_path: &str) -> (O, Proof) {
@@ -42,12 +39,25 @@ pub fn dispatch(test: &TestCase) -> Result<(), Error> {
                         test,
                         load_test,
                         |(mut object, proof): (spec::BeaconBlockBody, Proof), _| {
+                            let root = object.hash_tree_root().unwrap();
+                            // test proof matches
+                            // NOTE: `0` index hard-coded in test generator
+                            let path = &["blob_kzg_commitments".into(), 0.into()];
+                            let (computed_proof, witness) = prove(&mut object, path).expect("can prove");
+                            assert_eq!(root, witness);
+                            assert_eq!(proof.leaf, computed_proof.leaf);
+                            assert_eq!(proof.leaf_index, computed_proof.index);
+                            assert_eq!(proof.branch, computed_proof.branch);
+                            assert!(computed_proof.verify(witness).is_ok());
+                            // test generalized index verifier
+                            assert!(is_valid_merkle_branch_for_generalized_index(proof.leaf, &proof.branch, proof.leaf_index, root).is_ok());
+                            // test regular index verifier
                             is_valid_merkle_branch(
                                 proof.leaf,
                                 &proof.branch,
                                 log_2(proof.leaf_index).unwrap() as usize,
                                 get_subtree_index(proof.leaf_index).unwrap(),
-                                object.hash_tree_root().unwrap()
+                                root,
                             ).map_err(|err| SpecError::from(err).into())
                         }
                     }
