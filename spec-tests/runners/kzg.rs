b67f3d5487e697082de88365bcd8a9c80d3d45a5
EchoAlice <echoalice0@gmail.com>
2024-04-03 14:57:06 -0600

    Create `compute_kzg_proof` handler

--- spec-tests/runners/kzg.rs
+++ spec-tests/runners/kzg.rs
@@ -5,8 +5,9 @@ use crate::{
 use ethereum_consensus::deneb::{
     mainnet::Blob,
     polynomial_commitments::{
-        blob_to_kzg_commitment, kzg_settings_from_json, CKzgError,
-        Error as PolynomialCommitmentsError, KzgCommitment,
+        blob_to_kzg_commitment, compute_kzg_proof, kzg_settings_from_json, CKzgError,
+        Error as PolynomialCommitmentsError, FieldElement, KzgCommitment, KzgProof,
+        ProofAndEvaluation,
     },
     presets::TRUSTED_SETUP_JSON,
 };
@@ -21,9 +22,9 @@ pub fn dispatch(test: &TestCase) -> Result<(), Error> {
             // Load test case ----
             let path = path.to_string() + "/data.yaml";
             let test_data: serde_yaml::Value = load_yaml(&path);
-            let input = test_data.get("input").unwrap();
+            let input_yaml = test_data.get("input").unwrap();
             let output_yaml = test_data.get("output").unwrap();
-            let blob_yaml = input.get("blob").unwrap();
+            let blob_yaml = input_yaml.get("blob").unwrap();
 
             let input_result: Result<Blob, _> = serde_yaml::from_value(blob_yaml.clone());
             let output_result: Result<Option<KzgCommitment>, _> =
@@ -51,7 +52,46 @@ pub fn dispatch(test: &TestCase) -> Result<(), Error> {
             }
         }
         "compute_kzg_proof" => {
-            todo!()
+            // Load test case ----
+            let path = path.to_string() + "/data.yaml";
+            let test_data: serde_yaml::Value = load_yaml(&path);
+            let input_yaml = test_data.get("input").unwrap();
+            let output_yaml = test_data.get("output").unwrap();
+            let blob_yaml = input_yaml.get("blob").unwrap();
+            let z_yaml = input_yaml.get("z").unwrap();
+
+            let input_blob_result: Result<Blob, _> = serde_yaml::from_value(blob_yaml.clone());
+            let input_z_result: Result<FieldElement, _> = serde_yaml::from_value(z_yaml.clone());
+            let output_result: Result<Option<(KzgProof, FieldElement)>, _> =
+                serde_yaml::from_value(output_yaml.clone());
+
+            match (input_blob_result, input_z_result, output_result) {
+                // All maps for yaml file deserialized correctly
+                (Ok(blob), Ok(z), Ok(Some(expected))) => {
+                    let proof_and_evaluation = compute_kzg_proof(&blob, &z, &kzg_settings).unwrap();
+                    let expected = ProofAndEvaluation { proof: expected.0, evaluation: expected.1 };
+                    assert_eq!(proof_and_evaluation, expected);
+                    Ok(())
+                }
+                (Err(_), Ok(_), Ok(None)) => {
+                    // Expected state for failed test case - invalid length blob
+                    Ok(())
+                }
+                (Ok(blob), Ok(z), Ok(None)) => {
+                    let result = compute_kzg_proof(&blob, &z, &kzg_settings);
+                    assert!(matches!(
+                        result,
+                        Err(PolynomialCommitmentsError::CKzg(CKzgError::CError(..)))
+                    ));
+                    Ok(())
+                }
+
+                (Ok(_), Err(_), Ok(None)) => {
+                    // Expected state for failed test case - invalid evaluation point
+                    Ok(())
+                }
+                _ => unreachable!("not possible"),
+            }
         }
         "verify_kzg_proof" => {
             todo!()
@@ -68,27 +108,3 @@ pub fn dispatch(test: &TestCase) -> Result<(), Error> {
         _ => todo!(),
     }
 }
-/*
-#[derive(Debug, Deserialize)]
-pub struct BlobToKzgCommitmentTestCase {
-    input: spec::Blob,
-    output: Option<KzgCommitment>,
-}
-
-impl BlobToKzgCommitmentTestCase {
-    pub fn from(test_case_path: &str) -> Self {
-        let path = test_case_path.to_string() + "/data.yaml";
-        load_yaml(&path)
-    }
-
-    pub fn run(
-        &self,
-        blob: &spec::Blob,
-        kzg_settings: &polynomial_commitments::KzgSettings,
-    ) -> bool {
-        let kzg_commitment = blob_to_kzg_commitment(blob, kzg_settings).unwrap();
-        println!("Kzg commitment: {}", kzg_commitment);
-        &kzg_commitment == self.output.as_ref().unwrap()
-    }
-}
- */
