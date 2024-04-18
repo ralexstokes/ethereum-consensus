2a5d78191830f455082dc5e15ea45e2fcc544138
Alex Stokes <r.alex.stokes@gmail.com>
2024-04-18 15:45:15 -0600

    add electra genesis

--- ethereum-consensus/src/electra/genesis.rs
+++ ethereum-consensus/src/electra/genesis.rs
@@ -1,6 +1,11 @@
 use crate::{
-    electra::{BeaconState, Deposit, ExecutionPayloadHeader},
-    primitives::Hash32,
+    electra::{
+        get_next_sync_committee, increase_balance, process_deposit, BeaconBlockBody,
+        BeaconBlockHeader, BeaconState, Deposit, DepositData, Eth1Data, ExecutionPayloadHeader,
+        Fork, DEPOSIT_DATA_LIST_BOUND,
+    },
+    primitives::{Gwei, Hash32, GENESIS_EPOCH},
+    ssz::prelude::*,
     state_transition::{Context, Result},
 };
 
@@ -17,6 +22,8 @@ pub fn initialize_beacon_state_from_eth1<
     const PENDING_PARTIAL_WITHDRAWALS_LIMIT: usize,
     const PENDING_CONSOLIDATIONS_LIMIT: usize,
     const MAX_PROPOSER_SLASHINGS: usize,
+    const MAX_VALIDATORS_PER_SLOT: usize,
+    const MAX_COMMITTEES_PER_SLOT: usize,
     const MAX_ATTESTER_SLASHINGS: usize,
     const MAX_ATTESTATIONS: usize,
     const MAX_DEPOSITS: usize,
@@ -26,16 +33,19 @@ pub fn initialize_beacon_state_from_eth1<
     const MAX_BYTES_PER_TRANSACTION: usize,
     const MAX_TRANSACTIONS_PER_PAYLOAD: usize,
     const MAX_WITHDRAWALS_PER_PAYLOAD: usize,
+    const MAX_DEPOSIT_RECEIPTS_PER_PAYLOAD: usize,
+    const MAX_WITHDRAWAL_REQUESTS_PER_PAYLOAD: usize,
     const MAX_BLS_TO_EXECUTION_CHANGES: usize,
     const MAX_BLOB_COMMITMENTS_PER_BLOCK: usize,
+    const MAX_CONSOLIDATIONS: usize,
 >(
-    _eth1_block_hash: Hash32,
-    _eth1_timestamp: u64,
-    _deposits: &[Deposit],
-    _execution_payload_header: Option<
+    eth1_block_hash: Hash32,
+    eth1_timestamp: u64,
+    deposits: &[Deposit],
+    execution_payload_header: Option<
         &ExecutionPayloadHeader<BYTES_PER_LOGS_BLOOM, MAX_EXTRA_DATA_BYTES>,
     >,
-    _context: &Context,
+    context: &Context,
 ) -> Result<
     BeaconState<
         SLOTS_PER_HISTORICAL_ROOT,
@@ -53,5 +63,86 @@ pub fn initialize_beacon_state_from_eth1<
         PENDING_CONSOLIDATIONS_LIMIT,
     >,
 > {
-    todo!()
+    let fork = Fork {
+        previous_version: context.electra_fork_version,
+        current_version: context.electra_fork_version,
+        epoch: GENESIS_EPOCH,
+    };
+    let eth1_data = Eth1Data {
+        block_hash: eth1_block_hash.clone(),
+        deposit_count: deposits.len() as u64,
+        ..Default::default()
+    };
+    let latest_block_body = BeaconBlockBody::<
+        MAX_PROPOSER_SLASHINGS,
+        MAX_VALIDATORS_PER_SLOT,
+        MAX_COMMITTEES_PER_SLOT,
+        MAX_ATTESTER_SLASHINGS,
+        MAX_ATTESTATIONS,
+        MAX_DEPOSITS,
+        MAX_VOLUNTARY_EXITS,
+        SYNC_COMMITTEE_SIZE,
+        BYTES_PER_LOGS_BLOOM,
+        MAX_EXTRA_DATA_BYTES,
+        MAX_BYTES_PER_TRANSACTION,
+        MAX_TRANSACTIONS_PER_PAYLOAD,
+        MAX_WITHDRAWALS_PER_PAYLOAD,
+        MAX_DEPOSIT_RECEIPTS_PER_PAYLOAD,
+        MAX_WITHDRAWAL_REQUESTS_PER_PAYLOAD,
+        MAX_BLS_TO_EXECUTION_CHANGES,
+        MAX_BLOB_COMMITMENTS_PER_BLOCK,
+        MAX_CONSOLIDATIONS,
+    >::default();
+    let body_root = latest_block_body.hash_tree_root()?;
+    let latest_block_header = BeaconBlockHeader { body_root, ..Default::default() };
+    let randao_mixes = Vector::try_from(
+        std::iter::repeat(eth1_block_hash)
+            .take(context.epochs_per_historical_vector as usize)
+            .collect::<Vec<_>>(),
+    )
+    .map_err(|(_, err)| err)?;
+    let execution_payload_header = execution_payload_header.cloned().unwrap_or_default();
+    let mut state = BeaconState {
+        genesis_time: eth1_timestamp + context.genesis_delay,
+        fork,
+        eth1_data,
+        latest_block_header,
+        randao_mixes,
+        latest_execution_payload_header: execution_payload_header,
+        ..Default::default()
+    };
+
+    let mut leaves = List::<DepositData, DEPOSIT_DATA_LIST_BOUND>::default();
+    for deposit in deposits.iter() {
+        leaves.push(deposit.data.clone());
+        state.eth1_data.deposit_root = leaves.hash_tree_root()?;
+        process_deposit(&mut state, deposit, context)?;
+    }
+
+    let pending_balance_deposits = std::mem::take(&mut state.pending_balance_deposits);
+    for deposit in pending_balance_deposits.iter() {
+        increase_balance(&mut state, deposit.index, deposit.amount);
+    }
+
+    for i in 0..state.validators.len() {
+        let validator = &mut state.validators[i];
+        let balance = state.balances[i];
+        let effective_balance = Gwei::min(
+            balance - balance % context.effective_balance_increment,
+            context.max_effective_balance,
+        );
+        validator.effective_balance = effective_balance;
+        if validator.effective_balance == context.max_effective_balance {
+            validator.activation_eligibility_epoch = GENESIS_EPOCH;
+            validator.activation_epoch = GENESIS_EPOCH;
+        }
+    }
+
+    state.genesis_validators_root = state.validators.hash_tree_root()?;
+
+    let sync_committee = get_next_sync_committee(&state, context)?;
+    state.current_sync_committee = sync_committee.clone();
+    state.next_sync_committee = sync_committee;
+
+    Ok(state)
 }
