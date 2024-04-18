7bb38137d599db5e242d292761a3574c4b4ade7d
Alex Stokes <r.alex.stokes@gmail.com>
2024-04-18 11:56:33 -0600

    Write helpers for electra

--- ethereum-consensus/src/electra/mod.rs
+++ ethereum-consensus/src/electra/mod.rs
@@ -1,8 +1,10 @@
 pub mod beacon_block;
 pub mod beacon_state;
+pub mod block_processing;
 pub mod constants;
 pub mod execution_payload;
 pub mod fork;
+pub mod genesis;
 pub mod helpers;
 pub mod operations;
 pub mod presets;
