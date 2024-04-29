05fe2ebecf1ee03370769562d58d4e8fdd5bdb98
EchoAlice <echoalice0@gmail.com>
2024-04-29 14:16:34 -0600

    Rebase

--- ethereum-consensus/src/electra/mod.rs
+++ ethereum-consensus/src/electra/mod.rs
@@ -2,11 +2,7 @@ pub mod beacon_block;
 pub mod beacon_state;
 pub mod block_processing;
 pub mod constants;
-<<<<<<< HEAD
-pub mod execution_engine;
-=======
 pub mod epoch_processing;
->>>>>>> 3b086684 (Create `epoch_processing` module and `process_registry_updates()`)
 pub mod execution_payload;
 pub mod fork;
 pub mod genesis;
