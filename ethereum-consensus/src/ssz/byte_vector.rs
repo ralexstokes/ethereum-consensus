23c692ea99410da3812f40e5e2138934037b5209
Bob Liu <akagi201@gmail.com>
2024-10-08 22:01:49 +0800

    feat: add conversion between ByteVector and FixedBytes

--- ethereum-consensus/src/ssz/byte_vector.rs
+++ ethereum-consensus/src/ssz/byte_vector.rs
@@ -2,6 +2,7 @@ use crate::{
     serde::{write_bytes_to_lower_hex, write_bytes_to_lower_hex_display},
     ssz::prelude::*,
 };
+use alloy_primitives::FixedBytes;
 use std::{
     fmt,
     hash::{Hash, Hasher},
@@ -26,6 +27,20 @@ impl<const N: usize> TryFrom<&[u8]> for ByteVector<N> {
 
     fn try_from(bytes: &[u8]) -> Result<Self, Self::Error> {
         ByteVector::<N>::deserialize(bytes)
+    }
+}
+
+impl<const N: usize> TryFrom<FixedBytes<N>> for ByteVector<N> {
+    type Error = DeserializeError;
+
+    fn try_from(bytes: FixedBytes<N>) -> Result<Self, Self::Error> {
+        bytes.as_slice().try_into()
+    }
+}
+
+impl<const N: usize> From<ByteVector<N>> for FixedBytes<N> {
+    fn from(bytes: ByteVector<N>) -> Self {
+        FixedBytes::from_slice(bytes.as_ref())
     }
 }
 
@@ -64,5 +79,20 @@ impl<const N: usize> Deref for ByteVector<N> {
 impl<const N: usize> DerefMut for ByteVector<N> {
     fn deref_mut(&mut self) -> &mut Self::Target {
         &mut self.0
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    // test conversion between ByteVector and FixedBytes
+    #[test]
+    fn test_byte_vector_fixed_bytes_conversion() {
+        let bytes = [255u8; 32];
+        let byte_vector = ByteVector::<32>::try_from(&bytes[..]).unwrap();
+        let fixed_bytes = FixedBytes::from_slice(&bytes);
+        assert_eq!(byte_vector, fixed_bytes.try_into().unwrap());
+        assert_eq!(fixed_bytes, FixedBytes::from(byte_vector));
     }
 }
